0251d2401f2f33e5512ac1d65f92e2f18a28c895
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-01-24 11:10:00 +0700

    types: update typings

--- packages/osu-rebalance-difficulty-calculator/typings/index.d.ts
+++ packages/osu-rebalance-difficulty-calculator/typings/index.d.ts
@@ -318,6 +318,10 @@ declare module "@rian8337/osu-rebalance-difficulty-calculator" {
             stats?: MapStats;
         }): this;
         /**
+         * Generates difficulty hitobjects for this calculator.
+         */
+        override generateDifficultyHitObjects(): void;
+        /**
          * Calculates the aim star rating of the beatmap and stores it in this instance.
          */
         calculateAim(): void;
@@ -614,6 +618,10 @@ declare module "@rian8337/osu-rebalance-difficulty-calculator" {
              */
             stats?: MapStats;
         }): this;
+        /**
+         * Generates difficulty hitobjects for this calculator.
+         */
+        override generateDifficultyHitObjects(): void;
         /**
          * Calculates the aim star rating of the beatmap and stores it in this instance.
          */
@@ -1045,7 +1053,7 @@ declare module "@rian8337/osu-rebalance-difficulty-calculator" {
          *
          * @param mode The gamemode to generate difficulty hitobjects for.
          */
-        generateDifficultyHitObjects(mode: modes): void;
+        protected generateDifficultyHitObjects(mode: modes): void;
         /**
          * Calculates the skills provided.
          *
