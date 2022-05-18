bd59683f06cddd06aad20b186f9feba0cd16150d
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-05-18 15:01:05 +0700

    refactor(osu-base; BeatmapControlPointsDecoder): remove `addControlPoint`

--- packages/osu-base/src/beatmap/decoder/beatmap/BeatmapControlPointsDecoder.ts
+++ packages/osu-base/src/beatmap/decoder/beatmap/BeatmapControlPointsDecoder.ts
@@ -1,8 +1,6 @@
 import { EffectFlags } from "../../../constants/EffectFlags";
 import { SampleBank } from "../../../constants/SampleBank";
 import { Beatmap } from "../../Beatmap";
-import { ControlPoint } from "../../timings/ControlPoint";
-import { ControlPointManager } from "../../timings/ControlPointManager";
 import { DifficultyControlPoint } from "../../timings/DifficultyControlPoint";
 import { EffectControlPoint } from "../../timings/EffectControlPoint";
 import { SampleControlPoint } from "../../timings/SampleControlPoint";
@@ -66,58 +64,37 @@ export class BeatmapControlPointsDecoder extends SectionDecoder<Beatmap> {
         }
 
         if (msPerBeat >= 0) {
-            this.addControlPoint(
+            this.target.controlPoints.timing.add(
                 new TimingControlPoint({
                     time: time,
                     msPerBeat: msPerBeat,
                     timeSignature: timeSignature,
-                }),
-                this.target.controlPoints.timing
+                })
             );
         }
 
-        this.addControlPoint(
+        this.target.controlPoints.difficulty.add(
             new DifficultyControlPoint({
                 time: time,
                 speedMultiplier: msPerBeat < 0 ? 100 / -msPerBeat : 1,
-            }),
-            this.target.controlPoints.difficulty
+            })
         );
 
-        this.addControlPoint(
+        this.target.controlPoints.effect.add(
             new EffectControlPoint({
                 time: time,
                 isKiai: kiaiMode,
                 omitFirstBarLine: omitFirstBarSignature,
-            }),
-            this.target.controlPoints.effect
+            })
         );
 
-        this.addControlPoint(
+        this.target.controlPoints.sample.add(
             new SampleControlPoint({
                 time: time,
                 sampleBank: sampleSet,
                 sampleVolume: sampleVolume,
                 customSampleBank: customSampleBank,
-            }),
-            this.target.controlPoints.sample
+            })
         );
-    }
-
-    /**
-     * Adds a control point.
-     *
-     * @param controlPoint The control point to add.
-     */
-    private addControlPoint<T extends ControlPoint>(
-        controlPoint: T,
-        manager: ControlPointManager<T>
-    ): void {
-        // Remove the last control point if another control point overrides it at the same time.
-        while (manager.points.at(-1)?.time === controlPoint.time) {
-            manager.points.pop();
-        }
-
-        manager.add(controlPoint);
     }
 }
