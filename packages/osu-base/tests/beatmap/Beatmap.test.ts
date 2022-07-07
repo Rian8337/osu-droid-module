90bc6b7347c594bb85fbe5f38f3e044e0df05997
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-07 17:06:57 +0700

    feat(osu-base; Beatmap): improve test coverage

--- packages/osu-base/tests/beatmap/Beatmap.test.ts
+++ packages/osu-base/tests/beatmap/Beatmap.test.ts
@@ -5,6 +5,7 @@ import {
     EffectControlPoint,
     MapStats,
     ModHidden,
+    ModRelax,
     objectTypes,
     PathType,
     SampleBank,
@@ -247,40 +248,86 @@ test("Test sample control point getter", () => {
     expect(timingPoint.time).toBe(5000);
 });
 
-test("Test most common beat length getter", () => {
-    const beatmap = new Beatmap();
+describe("Test most common beat length getter", () => {
+    test("Without hitobjects and timing points", () => {
+        const beatmap = new Beatmap();
 
-    expect(beatmap.mostCommonBeatLength).toBe(0);
+        expect(beatmap.mostCommonBeatLength).toBe(0);
+    });
 
-    beatmap.controlPoints.timing.add(
-        new TimingControlPoint({
-            time: 1000,
-            msPerBeat: 1000,
-            timeSignature: 4,
-        })
-    );
+    test("With a timing point", () => {
+        const beatmap = new Beatmap();
 
-    expect(beatmap.mostCommonBeatLength).toBe(1000);
+        beatmap.controlPoints.timing.add(
+            new TimingControlPoint({
+                time: 1000,
+                msPerBeat: 1000,
+                timeSignature: 4,
+            })
+        );
 
-    beatmap.controlPoints.timing.add(
-        new TimingControlPoint({
-            time: 1500,
-            msPerBeat: 800,
-            timeSignature: 4,
-        })
-    );
+        expect(beatmap.mostCommonBeatLength).toBe(1000);
+    });
 
-    // Objects are not added yet, so most common beat length should still default to the first timing point.
-    expect(beatmap.mostCommonBeatLength).toBe(1000);
+    describe("With 2 timing points and a hitobject", () => {
+        test("Hitobject before timing points", () => {
+            const beatmap = new Beatmap();
 
-    beatmap.hitObjects.add(
-        new Slider({
-            ...createGlobalSliderValues(),
-            startTime: 1500,
-        })
-    );
+            beatmap.controlPoints.timing.add(
+                new TimingControlPoint({
+                    time: 1000,
+                    msPerBeat: 1000,
+                    timeSignature: 4,
+                })
+            );
 
-    expect(beatmap.mostCommonBeatLength).toBe(800);
+            beatmap.controlPoints.timing.add(
+                new TimingControlPoint({
+                    time: 1500,
+                    msPerBeat: 800,
+                    timeSignature: 4,
+                })
+            );
+
+            beatmap.hitObjects.add(
+                new Circle({
+                    startTime: 0,
+                    position: new Vector2(0, 0),
+                })
+            );
+
+            expect(beatmap.mostCommonBeatLength).toBe(1000);
+        });
+
+        test("Hitobject after timing points", () => {
+            const beatmap = new Beatmap();
+
+            beatmap.controlPoints.timing.add(
+                new TimingControlPoint({
+                    time: 1000,
+                    msPerBeat: 1000,
+                    timeSignature: 4,
+                })
+            );
+
+            beatmap.controlPoints.timing.add(
+                new TimingControlPoint({
+                    time: 1500,
+                    msPerBeat: 800,
+                    timeSignature: 4,
+                })
+            );
+
+            beatmap.hitObjects.add(
+                new Circle({
+                    startTime: 2000,
+                    position: new Vector2(0, 0),
+                })
+            );
+
+            expect(beatmap.mostCommonBeatLength).toBe(1000);
+        });
+    });
 });
 
 test("Test slider ticks getter", () => {
@@ -403,40 +450,156 @@ test("Test max combo getter", () => {
     expect(beatmap.maxCombo).toBe(5);
 });
 
-test("Test osu!droid max score calculation", () => {
+describe("Test osu!droid max score calculation", () => {
     const beatmap = new Beatmap();
-
-    const stats = new MapStats();
-
-    expect(beatmap.maxDroidScore(stats)).toBe(0);
 
     beatmap.hitObjects.add(
         new Circle({
             startTime: 1000,
             position: new Vector2(0, 0),
+        }),
+        new Slider({
+            ...createGlobalSliderValues(),
+            startTime: 1500,
         })
     );
 
-    expect(beatmap.maxDroidScore(stats)).toBe(300);
+    test("Without mods and speed multiplier", () => {
+        const stats = new MapStats();
 
-    stats.mods.push(new ModHidden());
+        expect(beatmap.maxDroidScore(stats)).toBe(730);
+    });
 
-    expect(beatmap.maxDroidScore(stats)).toBeCloseTo(300);
+    test("With mods", () => {
+        const stats = new MapStats({
+            mods: [new ModHidden()],
+        });
+
+        expect(beatmap.maxDroidScore(stats)).toBe(735);
+    });
+
+    test("With speed multiplier > 1", () => {
+        const stats = new MapStats({
+            speedMultiplier: 1.25,
+        });
+
+        expect(beatmap.maxDroidScore(stats)).toBe(735);
+    });
+
+    test("With speed multiplier < 1", () => {
+        const stats = new MapStats({
+            speedMultiplier: 0.75,
+        });
+
+        expect(beatmap.maxDroidScore(stats)).toBe(667);
+    });
+
+    test("With unranked mods", () => {
+        const stats = new MapStats({
+            mods: [new ModRelax()],
+        });
+
+        expect(beatmap.maxDroidScore(stats)).toBe(640);
+    });
 });
 
-test("Test osu!standard max score calculation", () => {
-    const beatmap = new Beatmap();
+describe("Test osu!standard max score calculation", () => {
+    const constructBeatmap = () => {
+        const beatmap = new Beatmap();
 
-    expect(beatmap.maxOsuScore()).toBe(0);
+        beatmap.hitObjects.add(
+            new Circle({
+                startTime: 1000,
+                position: new Vector2(0, 0),
+            })
+        );
 
-    beatmap.hitObjects.add(
-        new Circle({
-            startTime: 1000,
-            position: new Vector2(0, 0),
-        })
-    );
+        return beatmap;
+    };
 
-    expect(beatmap.maxOsuScore()).toBe(300);
+    test("Difficulty multiplier: 2", () => {
+        const beatmap = constructBeatmap();
 
-    expect(beatmap.maxOsuScore([new ModHidden()])).toBeCloseTo(300);
+        beatmap.difficulty.cs = 1;
+        beatmap.difficulty.od = 1;
+        beatmap.difficulty.hp = 1;
+
+        expect(beatmap.maxOsuScore()).toBe(300);
+    });
+
+    test("Difficulty multiplier: 3", () => {
+        const beatmap = constructBeatmap();
+
+        beatmap.difficulty.cs = 2;
+        beatmap.difficulty.od = 2;
+        beatmap.difficulty.hp = 2;
+
+        expect(beatmap.maxOsuScore()).toBe(300);
+    });
+
+    test("Difficulty multiplier: 4", () => {
+        const beatmap = constructBeatmap();
+
+        beatmap.difficulty.cs = 5;
+        beatmap.difficulty.od = 5;
+        beatmap.difficulty.hp = 5;
+
+        expect(beatmap.maxOsuScore()).toBe(300);
+    });
+
+    test("Difficulty multiplier: 5", () => {
+        const beatmap = constructBeatmap();
+
+        beatmap.difficulty.cs = 6;
+        beatmap.difficulty.od = 6;
+        beatmap.difficulty.hp = 6;
+
+        expect(beatmap.maxOsuScore()).toBe(300);
+    });
+
+    test("Difficulty multiplier: 6", () => {
+        const beatmap = constructBeatmap();
+
+        beatmap.difficulty.cs = 10;
+        beatmap.difficulty.od = 10;
+        beatmap.difficulty.hp = 10;
+
+        expect(beatmap.maxOsuScore()).toBe(300);
+    });
+
+    test("With mods", () => {
+        expect(constructBeatmap().maxOsuScore([new ModHidden()])).toBe(300);
+    });
+});
+
+describe("Test string concatenation", () => {
+    const constructBeatmap = () => {
+        const beatmap = new Beatmap();
+
+        beatmap.difficulty.ar = 5;
+
+        beatmap.metadata.artist = "A";
+        beatmap.metadata.title = "B";
+        beatmap.metadata.creator = "C";
+        beatmap.metadata.version = "D";
+
+        return beatmap;
+    };
+
+    test("Without title unicode", () => {
+        expect(constructBeatmap().toString()).toBe(
+            "A - B [D] mapped by C\n\nAR5 OD5 CS5 HP5\n0 circles, 0 sliders, 0 spinners\n0 max combo"
+        );
+    });
+
+    test("With title unicode", () => {
+        const beatmap = constructBeatmap();
+
+        beatmap.metadata.artistUnicode = "E";
+        beatmap.metadata.titleUnicode = "F";
+
+        expect(beatmap.toString()).toBe(
+            "A - B [(E - F)D] mapped by C\n\nAR5 OD5 CS5 HP5\n0 circles, 0 sliders, 0 spinners\n0 max combo"
+        );
+    });
 });
