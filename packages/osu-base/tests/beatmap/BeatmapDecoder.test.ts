f8d0d646484253bf358752f678d9aca29784cda7
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-07 18:13:48 +0700

    feat(osu-base; BeatmapDecoder): improve test coverage

--- packages/osu-base/tests/beatmap/BeatmapDecoder.test.ts
+++ packages/osu-base/tests/beatmap/BeatmapDecoder.test.ts
@@ -3,6 +3,7 @@ import {
     BeatmapCountdown,
     BeatmapDecoder,
     BeatmapOverlayPosition,
+    Circle,
     EditorGridSize,
     GameMode,
     RGBColor,
@@ -13,10 +14,23 @@ import {
 import { readFile } from "fs/promises";
 import { join } from "path";
 
-let beatmap = new Beatmap();
+let v3Beatmap = new Beatmap();
+let v14Beatmap = new Beatmap();
+let beatmapWithStoryboard = new Beatmap();
 
 beforeAll(async () => {
-    const data = await readFile(
+    const v3Data = await readFile(
+        join(
+            process.cwd(),
+            "tests",
+            "files",
+            "beatmaps",
+            "Kenji Ninuma - DISCOPRINCE (peppy) [Normal].osu"
+        ),
+        { encoding: "utf-8" }
+    );
+
+    const v14Data = await readFile(
         join(
             process.cwd(),
             "tests",
@@ -27,152 +41,328 @@ beforeAll(async () => {
         { encoding: "utf-8" }
     );
 
-    beatmap = new BeatmapDecoder().decode(data).result;
+    const beatmapWithStoryboardData = await readFile(
+        join(
+            process.cwd(),
+            "tests",
+            "files",
+            "beatmaps",
+            "Himeringo - Yotsuya-san ni Yoroshiku (RLC) [Winber1's Extreme].osu"
+        ),
+        { encoding: "utf-8" }
+    );
+
+    v3Beatmap = new BeatmapDecoder().decode(v3Data).result;
+    v14Beatmap = new BeatmapDecoder().decode(v14Data).result;
+    beatmapWithStoryboard = new BeatmapDecoder().decode(
+        beatmapWithStoryboardData
+    ).result;
 });
 
-test("Test colors section", () => {
-    const { colors } = beatmap;
+describe("Test colors section", () => {
+    test("v3 file format", () => {
+        const { colors } = v3Beatmap;
 
-    expect(colors.combo.length).toBe(4);
-    expect(colors.combo[0]).toEqual(new RGBColor(98, 243, 255));
-    expect(colors.combo[1]).toEqual(new RGBColor(251, 170, 251));
-    expect(colors.combo[2]).toEqual(new RGBColor(102, 171, 255));
-    expect(colors.combo[3]).toEqual(new RGBColor(162, 205, 232));
-    expect(colors.sliderBorder).toBeUndefined();
-    expect(colors.sliderTrackOverride).toBeUndefined();
+        expect(colors.combo.length).toBe(0);
+    });
+
+    test("v14 file format", () => {
+        const { colors } = v14Beatmap;
+
+        expect(colors.combo.length).toBe(4);
+        expect(colors.combo[0]).toEqual(new RGBColor(98, 243, 255));
+        expect(colors.combo[1]).toEqual(new RGBColor(251, 170, 251));
+        expect(colors.combo[2]).toEqual(new RGBColor(102, 171, 255));
+        expect(colors.combo[3]).toEqual(new RGBColor(162, 205, 232));
+        expect(colors.sliderBorder).toBeUndefined();
+        expect(colors.sliderTrackOverride).toBeUndefined();
+    });
 });
 
-test("Test control points section", () => {
-    const { controlPoints } = beatmap;
+describe("Test control points section", () => {
+    test("v3 file format", () => {
+        const { controlPoints } = v3Beatmap;
 
-    expect(controlPoints.difficulty.points.length).toBe(14);
-    expect(controlPoints.effect.points.length).toBe(14);
-    expect(controlPoints.timing.points.length).toBe(5);
-    expect(controlPoints.sample.points.length).toBe(526);
+        expect(controlPoints.difficulty.points.length).toBe(0);
+        expect(controlPoints.effect.points.length).toBe(0);
+        expect(controlPoints.sample.points.length).toBe(0);
+        expect(controlPoints.timing.points.length).toBe(1);
+    });
+
+    test("v14 file format", () => {
+        const { controlPoints } = v14Beatmap;
+
+        expect(controlPoints.difficulty.points.length).toBe(14);
+        expect(controlPoints.effect.points.length).toBe(14);
+        expect(controlPoints.sample.points.length).toBe(526);
+        expect(controlPoints.timing.points.length).toBe(5);
+    });
 });
 
-test("Test difficulty section", () => {
-    const { difficulty } = beatmap;
+describe("Test difficulty section", () => {
+    test("v13 file format", () => {
+        const { difficulty } = v3Beatmap;
 
-    expect(difficulty.ar).toBe(9);
-    expect(difficulty.cs).toBe(4);
-    expect(difficulty.hp).toBe(5);
-    expect(difficulty.od).toBe(8);
-    expect(difficulty.sliderMultiplier).toBe(1.9);
-    expect(difficulty.sliderTickRate).toBe(1);
+        expect(difficulty.ar).toBe(6);
+        expect(difficulty.cs).toBe(4);
+        expect(difficulty.hp).toBe(6);
+        expect(difficulty.od).toBe(6);
+        expect(difficulty.sliderMultiplier).toBe(1.4);
+        expect(difficulty.sliderTickRate).toBe(2);
+    });
+
+    test("v14 file format", () => {
+        const { difficulty } = v14Beatmap;
+
+        expect(difficulty.ar).toBe(9);
+        expect(difficulty.cs).toBe(4);
+        expect(difficulty.hp).toBe(5);
+        expect(difficulty.od).toBe(8);
+        expect(difficulty.sliderMultiplier).toBe(1.9);
+        expect(difficulty.sliderTickRate).toBe(1);
+    });
 });
 
-test("Test editor section", () => {
-    const { editor } = beatmap;
+describe("Test editor section", () => {
+    test("v3 file format", () => {
+        const { editor } = v3Beatmap;
 
-    expect(editor.beatDivisor).toBe(4);
-    expect(editor.bookmarks.length).toBe(0);
-    expect(editor.distanceSnap).toBe(0.2);
-    expect(editor.gridSize).toBe(EditorGridSize.small);
-    expect(editor.timelineZoom).toBeCloseTo(3);
+        expect(editor.beatDivisor).toBe(4);
+        expect(editor.bookmarks.length).toBe(0);
+        expect(editor.distanceSnap).toBe(1);
+        expect(editor.gridSize).toBe(EditorGridSize.small);
+        expect(editor.timelineZoom).toBe(1);
+    });
+
+    test("v14 file format", () => {
+        const { editor } = v14Beatmap;
+
+        expect(editor.beatDivisor).toBe(4);
+        expect(editor.bookmarks.length).toBe(0);
+        expect(editor.distanceSnap).toBe(0.2);
+        expect(editor.gridSize).toBe(EditorGridSize.small);
+        expect(editor.timelineZoom).toBeCloseTo(3);
+    });
 });
 
-test("Test events section", () => {
-    const { events } = beatmap;
+describe("Test events section", () => {
+    test("v3 file format", () => {
+        const { events } = v3Beatmap;
 
-    expect(events.background?.filename).toBe("school.jpg");
-    expect(events.background?.offset).toEqual(new Vector2(0, 0));
-    expect(events.breaks.length).toBe(0);
-    expect(events.video?.filename).toBe("Yoasobi.mp4");
-    expect(events.video?.offset).toEqual(new Vector2(0, 0));
+        expect(events.background?.filename).toBe("katamari2.jpg");
+        expect(events.background?.offset).toEqual(new Vector2(0, 0));
+        expect(events.breaks.length).toBe(3);
+        expect(events.video).toBeUndefined();
+    });
+
+    test("v14 file format", () => {
+        const { events } = v14Beatmap;
+
+        expect(events.background?.filename).toBe("school.jpg");
+        expect(events.background?.offset).toEqual(new Vector2(0, 0));
+        expect(events.breaks.length).toBe(0);
+        expect(events.video?.filename).toBe("Yoasobi.mp4");
+        expect(events.video?.offset).toEqual(new Vector2(0, 0));
+    });
 });
 
-test("Test format version header", () => {
-    expect(beatmap.formatVersion).toBe(14);
+describe("Test format version header", () => {
+    test("v3 file format", () => {
+        expect(v3Beatmap.formatVersion).toBe(3);
+    });
+
+    test("v14 file format", () => {
+        expect(v14Beatmap.formatVersion).toBe(14);
+    });
 });
 
-test("Test general section", () => {
-    const { general } = beatmap;
+describe("Test general section", () => {
+    test("v3 file format", () => {
+        const { general } = v3Beatmap;
 
-    expect(general.audioFilename).toBe("audio.mp3");
-    expect(general.audioLeadIn).toBe(0);
-    expect(general.countdown).toBe(BeatmapCountdown.noCountDown);
-    expect(general.countdownOffset).toBe(0);
-    expect(general.epilepsyWarning).toBe(false);
-    expect(general.letterBoxInBreaks).toBe(false);
-    expect(general.mode).toBe(GameMode.osu);
-    expect(general.overlayPosition).toBe(BeatmapOverlayPosition.noChange);
-    expect(general.previewTime).toBe(49037);
-    expect(general.sampleBank).toBe(SampleBank.soft);
-    expect(general.sampleVolume).toBe(100);
-    expect(general.samplesMatchPlaybackRate).toBe(true);
-    expect(general.skinPreference).toBe("");
-    expect(general.stackLeniency).toBe(0.2);
-    expect(general.useSkinSprites).toBe(false);
-    expect(general.widescreenStoryboard).toBe(true);
+        expect(general.audioFilename).toBe("20.mp3");
+        expect(general.audioLeadIn).toBe(0);
+        expect(general.countdown).toBe(BeatmapCountdown.normal);
+        expect(general.countdownOffset).toBe(0);
+        expect(general.epilepsyWarning).toBe(false);
+        expect(general.letterBoxInBreaks).toBe(false);
+        expect(general.mode).toBe(GameMode.osu);
+        expect(general.overlayPosition).toBe(BeatmapOverlayPosition.noChange);
+        expect(general.previewTime).toBe(-1);
+        expect(general.sampleBank).toBe(SampleBank.normal);
+        expect(general.sampleVolume).toBe(100);
+        expect(general.samplesMatchPlaybackRate).toBe(true);
+        expect(general.skinPreference).toBe("");
+        expect(general.stackLeniency).toBe(0.7);
+        expect(general.useSkinSprites).toBe(false);
+        expect(general.widescreenStoryboard).toBe(false);
+    });
+
+    test("v14 file format", () => {
+        const { general } = v14Beatmap;
+
+        expect(general.audioFilename).toBe("audio.mp3");
+        expect(general.audioLeadIn).toBe(0);
+        expect(general.countdown).toBe(BeatmapCountdown.noCountDown);
+        expect(general.countdownOffset).toBe(0);
+        expect(general.epilepsyWarning).toBe(false);
+        expect(general.letterBoxInBreaks).toBe(false);
+        expect(general.mode).toBe(GameMode.osu);
+        expect(general.overlayPosition).toBe(BeatmapOverlayPosition.noChange);
+        expect(general.previewTime).toBe(49037);
+        expect(general.sampleBank).toBe(SampleBank.soft);
+        expect(general.sampleVolume).toBe(100);
+        expect(general.samplesMatchPlaybackRate).toBe(true);
+        expect(general.skinPreference).toBe("");
+        expect(general.stackLeniency).toBe(0.2);
+        expect(general.useSkinSprites).toBe(false);
+        expect(general.widescreenStoryboard).toBe(true);
+    });
 });
 
-test("Test hitobject counters", () => {
-    const { hitObjects } = beatmap;
+describe("Test hitobject counters", () => {
+    test("v3 file format", () => {
+        const { hitObjects } = v3Beatmap;
 
-    expect(hitObjects.circles).toBe(198);
-    expect(hitObjects.objects.length).toBe(592);
-    expect(hitObjects.sliderEnds).toBe(393);
-    expect(hitObjects.sliderRepeatPoints).toBe(27);
-    expect(hitObjects.sliders).toBe(393);
-    expect(hitObjects.spinners).toBe(1);
+        expect(hitObjects.circles).toBe(160);
+        expect(hitObjects.objects.length).toBe(194);
+        expect(hitObjects.sliderEnds).toBe(30);
+        expect(hitObjects.sliderRepeatPoints).toBe(15);
+        expect(hitObjects.sliders).toBe(30);
+        expect(hitObjects.spinners).toBe(4);
+    });
+
+    test("v14 file format", () => {
+        const { hitObjects } = v14Beatmap;
+
+        expect(hitObjects.circles).toBe(198);
+        expect(hitObjects.objects.length).toBe(592);
+        expect(hitObjects.sliderEnds).toBe(393);
+        expect(hitObjects.sliderRepeatPoints).toBe(27);
+        expect(hitObjects.sliders).toBe(393);
+        expect(hitObjects.spinners).toBe(1);
+    });
 });
 
-test("Test hit object samples", () => {
-    const slider = <Slider>beatmap.hitObjects.objects[1];
+describe("Test hit object samples", () => {
+    test("v3 file format", () => {
+        const circle = <Circle>v3Beatmap.hitObjects.objects[0];
 
-    const [firstSample, lastSample] = slider.samples;
+        const [firstSample, lastSample] = circle.samples;
 
-    expect(slider.samples.length).toBe(2);
-    expect(firstSample.name).toBe("hitnormal");
-    expect(firstSample.bank).toBe(SampleBank.soft);
-    expect(firstSample.customSampleBank).toBe(0);
-    expect(firstSample.volume).toBe(0);
-    expect(firstSample.isLayered).toBe(true);
-    expect(lastSample.name).toBe("hitclap");
-    expect(lastSample.bank).toBe(SampleBank.drum);
-    expect(lastSample.customSampleBank).toBe(0);
-    expect(lastSample.volume).toBe(0);
-    expect(lastSample.isLayered).toBe(false);
-});
+        expect(circle.samples.length).toBe(2);
+        expect(firstSample.name).toBe("hitnormal");
+        expect(firstSample.bank).toBe(SampleBank.none);
+        expect(firstSample.customSampleBank).toBe(0);
+        expect(firstSample.volume).toBe(0);
+        expect(firstSample.isLayered).toBe(true);
+        expect(lastSample.name).toBe("hitfinish");
+        expect(lastSample.bank).toBe(SampleBank.none);
+        expect(lastSample.customSampleBank).toBe(0);
+        expect(lastSample.volume).toBe(0);
+        expect(lastSample.isLayered).toBe(false);
+    });
 
-test("Test hit object per-node samples", () => {
-    const slider = <Slider>beatmap.hitObjects.objects[1];
+    test("v14 file format", () => {
+        const slider = <Slider>v14Beatmap.hitObjects.objects[1];
 
-    for (const nodeSample of slider.nodeSamples) {
-        const [firstSample, lastSample] = nodeSample;
+        const [firstSample, lastSample] = slider.samples;
 
+        expect(slider.samples.length).toBe(2);
         expect(firstSample.name).toBe("hitnormal");
         expect(firstSample.bank).toBe(SampleBank.soft);
         expect(firstSample.customSampleBank).toBe(0);
         expect(firstSample.volume).toBe(0);
         expect(firstSample.isLayered).toBe(true);
-        expect(firstSample.isCustom).toBe(false);
-
         expect(lastSample.name).toBe("hitclap");
         expect(lastSample.bank).toBe(SampleBank.drum);
         expect(lastSample.customSampleBank).toBe(0);
         expect(lastSample.volume).toBe(0);
         expect(lastSample.isLayered).toBe(false);
-        expect(lastSample.isCustom).toBe(false);
-    }
+    });
 });
 
-test("Test max combo getter", () => {
-    expect(beatmap.maxCombo).toBe(1033);
+describe("Test hit object per-node samples", () => {
+    test("v3 file format", () => {
+        const slider = <Slider>v3Beatmap.hitObjects.objects[19];
+
+        for (const nodeSample of slider.nodeSamples) {
+            const [firstSample] = nodeSample;
+
+            expect(firstSample.name).toBe("hitnormal");
+            expect(firstSample.bank).toBe(SampleBank.none);
+            expect(firstSample.customSampleBank).toBe(0);
+            expect(firstSample.volume).toBe(0);
+            expect(firstSample.isLayered).toBe(false);
+            expect(firstSample.isCustom).toBe(false);
+        }
+    });
+
+    test("v14 file format", () => {
+        const slider = <Slider>v14Beatmap.hitObjects.objects[1];
+
+        for (const nodeSample of slider.nodeSamples) {
+            const [firstSample, lastSample] = nodeSample;
+
+            expect(firstSample.name).toBe("hitnormal");
+            expect(firstSample.bank).toBe(SampleBank.soft);
+            expect(firstSample.customSampleBank).toBe(0);
+            expect(firstSample.volume).toBe(0);
+            expect(firstSample.isLayered).toBe(true);
+            expect(firstSample.isCustom).toBe(false);
+
+            expect(lastSample.name).toBe("hitclap");
+            expect(lastSample.bank).toBe(SampleBank.drum);
+            expect(lastSample.customSampleBank).toBe(0);
+            expect(lastSample.volume).toBe(0);
+            expect(lastSample.isLayered).toBe(false);
+            expect(lastSample.isCustom).toBe(false);
+        }
+    });
 });
 
-test("Test metadata section", () => {
-    const { metadata } = beatmap;
+describe("Test max combo getter", () => {
+    test("v3 file format", () => {
+        expect(v3Beatmap.maxCombo).toBe(314);
+    });
 
-    expect(metadata.artist).toBe("YOASOBI");
-    expect(metadata.artistUnicode).toBe("YOASOBI");
-    expect(metadata.beatmapId).toBe(3324715);
-    expect(metadata.beatmapSetId).toBe(1585863);
-    expect(metadata.creator).toBe("ohm002");
-    expect(metadata.title).toBe("Love Letter");
-    expect(metadata.titleUnicode).toBe("ラブレター");
-    expect(metadata.version).toBe("Please accept my overflowing emotions.");
-    expect(metadata.source).toBe("");
-    expect(metadata.tags.length).toBe(27);
+    test("v14 file format", () => {
+        expect(v14Beatmap.maxCombo).toBe(1033);
+    });
+});
+
+describe("Test metadata section", () => {
+    test("v3 file format", () => {
+        const { metadata } = v3Beatmap;
+
+        expect(metadata.artist).toBe("Kenji Ninuma");
+        expect(metadata.artistUnicode).toBe("");
+        expect(metadata.beatmapId).toBeUndefined();
+        expect(metadata.beatmapSetId).toBeUndefined();
+        expect(metadata.creator).toBe("peppy");
+        expect(metadata.title).toBe("DISCO★PRINCE");
+        expect(metadata.titleUnicode).toBe("");
+        expect(metadata.version).toBe("Normal");
+        expect(metadata.source).toBe("");
+        expect(metadata.tags.length).toBe(0);
+    });
+
+    test("v14 file format", () => {
+        const { metadata } = v14Beatmap;
+
+        expect(metadata.artist).toBe("YOASOBI");
+        expect(metadata.artistUnicode).toBe("YOASOBI");
+        expect(metadata.beatmapId).toBe(3324715);
+        expect(metadata.beatmapSetId).toBe(1585863);
+        expect(metadata.creator).toBe("ohm002");
+        expect(metadata.title).toBe("Love Letter");
+        expect(metadata.titleUnicode).toBe("ラブレター");
+        expect(metadata.version).toBe("Please accept my overflowing emotions.");
+        expect(metadata.source).toBe("");
+        expect(metadata.tags.length).toBe(27);
+    });
+});
+
+test("Test storyboard decoding", () => {
+    expect(beatmapWithStoryboard.events.storyboard).toBeDefined();
 });
