6fa5d87f51634b12151b52b9987ef058e4e41998
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-08 10:56:08 +0700

    feat(osu-base; EffectControlPoint): add test

---
+++ packages/osu-base/tests/beatmap/timings/EffectControlPoint.test.ts
@@ -0,0 +1,37 @@
+import { EffectControlPoint } from "../../../src";
+
+test("Test timing point redundancy", () => {
+    const timingPoint = new EffectControlPoint({
+        time: 1000,
+        isKiai: false,
+        omitFirstBarLine: false,
+    });
+
+    let otherTimingPoint = new EffectControlPoint({
+        time: 1500,
+        isKiai: false,
+        omitFirstBarLine: false,
+    });
+
+    expect(timingPoint.isRedundant(otherTimingPoint)).toBe(true);
+
+    otherTimingPoint = new EffectControlPoint({
+        time: 1500,
+        isKiai: true,
+        omitFirstBarLine: false,
+    });
+
+    expect(timingPoint.isRedundant(otherTimingPoint)).toBe(false);
+});
+
+test("Test string concatenation", () => {
+    const timingPoint = new EffectControlPoint({
+        time: 1000,
+        isKiai: false,
+        omitFirstBarLine: false,
+    });
+
+    expect(timingPoint.toString()).toBe(
+        "{ time: 1000, kiai: false }"
+    );
+});
