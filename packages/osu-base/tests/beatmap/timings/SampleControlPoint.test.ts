4a71a494715782d0fc620e29d79cc1eeae4605a5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-08 10:52:56 +0700

    feat(osu-base; SampleControlPoint): add test

---
+++ packages/osu-base/tests/beatmap/timings/SampleControlPoint.test.ts
@@ -0,0 +1,41 @@
+import { SampleBank, SampleControlPoint } from "../../../src"
+
+test("Test timing point redundancy", () => {
+    const timingPoint = new SampleControlPoint({
+        time: 1000,
+        sampleBank: SampleBank.normal,
+        sampleVolume: 0,
+        customSampleBank: 0,
+    });
+
+    let otherTimingPoint = new SampleControlPoint({
+        time: 1500,
+        sampleBank: SampleBank.normal,
+        sampleVolume: 0,
+        customSampleBank: 0,
+    });
+
+    expect(timingPoint.isRedundant(otherTimingPoint)).toBe(true);
+
+    otherTimingPoint = new SampleControlPoint({
+        time: 1500,
+        sampleBank: SampleBank.drum,
+        sampleVolume: 0,
+        customSampleBank: 0,
+    });
+
+    expect(timingPoint.isRedundant(otherTimingPoint)).toBe(false);
+});
+
+test("Test string concatenation", () => {
+    const timingPoint = new SampleControlPoint({
+        time: 1000,
+        sampleBank: SampleBank.normal,
+        sampleVolume: 0,
+        customSampleBank: 0,
+    });
+
+    expect(timingPoint.toString()).toBe(
+        "{ time: 1000, sample bank: 1, sample volume: 0 }"
+    );
+});
