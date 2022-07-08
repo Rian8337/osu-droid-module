c64b7a7a1b2dfbef62962bc42421d8c760b9270c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-08 10:40:26 +0700

    feat(osu-base; BreakPoint): improve test coverage

--- packages/osu-base/tests/beatmap/timings/BreakPoint.test.ts
+++ packages/osu-base/tests/beatmap/timings/BreakPoint.test.ts
@@ -12,3 +12,14 @@ test("Test time coverage", () => {
     // Break point active time is not exactly at end time
     expect(breakPoint.contains(2750)).toBe(false);
 });
+
+test("Test string concatenation", () => {
+    const breakPoint = new BreakPoint({
+        startTime: 1000,
+        endTime: 3000,
+    });
+
+    expect(breakPoint.toString()).toBe(
+        "Start time: 1000, end time: 3000, duration: 2000"
+    );
+});
