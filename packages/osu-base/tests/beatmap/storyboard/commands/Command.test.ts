a323d3ab2e460755edabe8ceb98fa670ba61ef7b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-09 09:43:30 +0700

    feat(osu-base; Command): add test

---
+++ packages/osu-base/tests/beatmap/storyboard/commands/Command.test.ts
@@ -0,0 +1,44 @@
+import {
+    Command,
+    Easing,
+    StoryboardCommandType,
+    StoryboardParameterCommandType,
+} from "../../../../src";
+
+test("Test string concatenation", () => {
+    const command = new Command(
+        Easing.in,
+        0,
+        1000,
+        0,
+        1,
+        StoryboardCommandType.movementX
+    );
+
+    expect(command.toString()).toBe(`0 -> 1000, 0 -> 1 ${Easing.in}`);
+});
+
+test("Test isParameter typeguard", () => {
+    let command = new Command(
+        Easing.in,
+        0,
+        1000,
+        0,
+        1,
+        StoryboardCommandType.movementX
+    );
+
+    expect(command.isParameter()).toBe(false);
+
+    command = new Command(
+        Easing.in,
+        0,
+        1000,
+        0,
+        1,
+        StoryboardCommandType.movementX,
+        StoryboardParameterCommandType.blendingMode
+    );
+
+    expect(command.isParameter()).toBe(true);
+});
