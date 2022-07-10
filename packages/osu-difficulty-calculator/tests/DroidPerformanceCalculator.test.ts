5c58268a041f12bd902142cc7d8608cfc9a7e2ea
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-10 12:16:20 +0700

    feat(osu-difficulty-calculator; *PerformanceCalculator): add tests

---
+++ packages/osu-difficulty-calculator/tests/DroidPerformanceCalculator.test.ts
@@ -0,0 +1,471 @@
+import {
+    Accuracy,
+    Beatmap,
+    BeatmapDecoder,
+    MapStats,
+    ModDoubleTime,
+    ModFlashlight,
+    ModHidden,
+    ModNoFail,
+    ModRelax,
+    ModScoreV2,
+} from "@rian8337/osu-base";
+import { readFileSync } from "fs";
+import { join } from "path";
+import {
+    DifficultyCalculationOptions,
+    DroidDifficultyCalculator,
+    DroidPerformanceCalculator,
+    PerformanceCalculationOptions,
+} from "../src";
+
+const getBeatmapData = (path: string) => {
+    return readFileSync(
+        join(process.cwd(), "tests", "files", "beatmaps", path),
+        { encoding: "utf-8" }
+    );
+};
+
+const calculateDifficulty = (
+    beatmap: Beatmap,
+    options?: DifficultyCalculationOptions
+) => {
+    return new DroidDifficultyCalculator(beatmap).calculate(options);
+};
+
+const calculatePerformance = (
+    calculator: DroidDifficultyCalculator,
+    options?: PerformanceCalculationOptions
+) => {
+    return new DroidPerformanceCalculator(calculator).calculate(options);
+};
+
+const decoder = new BeatmapDecoder();
+
+const mainBeatmap = decoder.decode(
+    getBeatmapData(
+        "YOASOBI - Love Letter (ohm002) [Please accept my overflowing emotions.].osu"
+    )
+).result;
+
+const spinnerOnlyBeatmap = decoder.decode(
+    getBeatmapData("Calculate SPM - Spinner Test (Ash Marley) [Easy].osu")
+).result;
+
+describe("Test performance calculation with NoMod", () => {
+    const difficulty = calculateDifficulty(mainBeatmap);
+
+    test("SS", () => {
+        const performance = calculatePerformance(difficulty);
+
+        expect(performance.aim).toBeCloseTo(15.406637432317359, 5);
+        expect(performance.tap).toBeCloseTo(12.186444879347485, 5);
+        expect(performance.accuracy).toBeCloseTo(13.797682282371657, 5);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(27.149798544590933, 5);
+        expect(performance.total).toBeCloseTo(75.33580825676417, 5);
+    });
+
+    test("1 miss", () => {
+        const performance = calculatePerformance(difficulty, { miss: 1 });
+
+        expect(performance.aim).toBeCloseTo(14.813221125855863, 5);
+        expect(performance.tap).toBeCloseTo(11.454287763435078, 5);
+        expect(performance.accuracy).toBeCloseTo(12.984300892290696, 5);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(25.796968539816664, 5);
+        expect(performance.total).toBeCloseTo(71.50409352507185, 5);
+    });
+
+    test("99% approximated", () => {
+        const performance = calculatePerformance(difficulty, {
+            accPercent: new Accuracy({
+                percent: 99,
+                nobjects: mainBeatmap.hitObjects.objects.length,
+            }),
+        });
+
+        expect(performance.aim).toBeCloseTo(15.250489079962792, 5);
+        expect(performance.tap).toBeCloseTo(10.510665787842491, 5);
+        expect(performance.accuracy).toBeCloseTo(9.537588955661478, 5);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(25.024988453629206, 5);
+        expect(performance.total).toBeCloseTo(66.45809909307407, 5);
+    });
+
+    test("1.5 tap penalty", () => {
+        const performance = calculatePerformance(difficulty, {
+            tapPenalty: 1.5,
+        });
+
+        expect(performance.tap).toBeCloseTo(8.124296586231656, 5);
+        expect(performance.total).toBeCloseTo(71.11432510297956, 5);
+    });
+});
+
+describe("Test performance calculation with Hidden", () => {
+    const difficulty = calculateDifficulty(mainBeatmap, {
+        mods: [new ModHidden()],
+    });
+
+    test("SS", () => {
+        const performance = calculatePerformance(difficulty);
+
+        expect(performance.accuracy).toBeCloseTo(14.90149686496139, 5);
+        expect(performance.visual).toBeCloseTo(36.351952144791305, 5);
+        expect(performance.total).toBeCloseTo(87.12273380675724, 5);
+    });
+
+    test("1 miss", () => {
+        const performance = calculatePerformance(difficulty, { miss: 1 });
+
+        expect(performance.accuracy).toBeCloseTo(14.023044963673952, 5);
+        expect(performance.visual).toBeCloseTo(34.540593894275304, 5);
+        expect(performance.total).toBeCloseTo(82.69310755070373, 5);
+    });
+
+    test("99% approximated", () => {
+        const performance = calculatePerformance(difficulty, {
+            accPercent: new Accuracy({
+                percent: 99,
+                nobjects: mainBeatmap.hitObjects.objects.length,
+            }),
+        });
+
+        expect(performance.accuracy).toBeCloseTo(10.300596072114397, 5);
+        expect(performance.visual).toBeCloseTo(33.50695885261091, 5);
+        expect(performance.total).toBeCloseTo(77.07335849407274, 5);
+    });
+});
+
+describe("Test performance calculation with Flashlight", () => {
+    const difficulty = calculateDifficulty(mainBeatmap, {
+        mods: [new ModFlashlight()],
+    });
+
+    test("SS", () => {
+        const performance = calculatePerformance(difficulty);
+
+        expect(performance.flashlight).toBeCloseTo(4.289739291814374, 5);
+        expect(performance.total).toBeCloseTo(79.33046796152065, 5);
+    });
+
+    test("1 miss", () => {
+        const performance = calculatePerformance(difficulty, { miss: 1 });
+
+        expect(performance.flashlight).toBeCloseTo(4.128001157642297, 5);
+        expect(performance.total).toBeCloseTo(75.34641847863612, 5);
+    });
+
+    test("99% approximated", () => {
+        const performance = calculatePerformance(difficulty, {
+            accPercent: new Accuracy({
+                percent: 99,
+                nobjects: mainBeatmap.hitObjects.objects.length,
+            }),
+        });
+
+        expect(performance.flashlight).toBeCloseTo(4.268000748105856, 5);
+        expect(performance.total).toBeCloseTo(70.38157174092453, 5);
+    });
+});
+
+describe("Test performance calculation with Hidden, Flashlight", () => {
+    const difficulty = calculateDifficulty(mainBeatmap, {
+        mods: [new ModHidden(), new ModFlashlight()],
+    });
+
+    test("SS", () => {
+        const performance = calculatePerformance(difficulty);
+
+        expect(performance.flashlight).toBeCloseTo(5.90254677951125, 5);
+        expect(performance.total).toBeCloseTo(92.61650954594776, 5);
+    });
+
+    test("1 miss", () => {
+        const performance = calculatePerformance(difficulty, { miss: 1 });
+
+        expect(performance.flashlight).toBeCloseTo(5.6800001774827225, 5);
+        expect(performance.total).toBeCloseTo(87.97941878800835, 5);
+    });
+
+    test("99% approximated", () => {
+        const performance = calculatePerformance(difficulty, {
+            accPercent: new Accuracy({
+                percent: 99,
+                nobjects: mainBeatmap.hitObjects.objects.length,
+            }),
+        });
+
+        expect(performance.flashlight).toBeCloseTo(5.872635224885349, 5);
+        expect(performance.total).toBeCloseTo(82.49704578015316, 5);
+    });
+});
+
+describe("Test performance calculation with DoubleTime", () => {
+    const difficulty = calculateDifficulty(mainBeatmap, {
+        mods: [new ModDoubleTime()],
+    });
+
+    test("SS", () => {
+        const performance = calculatePerformance(difficulty);
+
+        expect(performance.aim).toBeCloseTo(36.87134437713346, 5);
+        expect(performance.tap).toBeCloseTo(40.534241959701404, 5);
+        expect(performance.accuracy).toBeCloseTo(45.13962922313779, 5);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(37.480247684471664, 5);
+        expect(performance.total).toBeCloseTo(174.9932577147693, 5);
+    });
+
+    test("1 miss", () => {
+        const performance = calculatePerformance(difficulty, { miss: 1 });
+
+        expect(performance.aim).toBeCloseTo(35.45117355201543, 5);
+        expect(performance.tap).toBeCloseTo(38.43243669000174, 5);
+        expect(performance.accuracy).toBeCloseTo(42.4786218442272, 5);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(35.61266831475194, 5);
+        expect(performance.total).toBeCloseTo(166.18093001273732, 5);
+    });
+
+    test("99% approximated", () => {
+        const performance = calculatePerformance(difficulty, {
+            accPercent: new Accuracy({
+                percent: 99,
+                nobjects: mainBeatmap.hitObjects.objects.length,
+            }),
+        });
+
+        expect(performance.aim).toBeCloseTo(36.49764831925712, 5);
+        expect(performance.tap).toBeCloseTo(36.862053117864846, 5);
+        expect(performance.accuracy).toBeCloseTo(31.202575934894714, 5);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(34.546951204908034, 5);
+        expect(performance.total).toBeCloseTo(152.10258850381857, 5);
+    });
+
+    test("1.5 tap penalty", () => {
+        const performance = calculatePerformance(difficulty, {
+            tapPenalty: 1.5,
+        });
+
+        expect(performance.tap).toBeCloseTo(27.02282797313427, 5);
+        expect(performance.total).toBeCloseTo(160.41778239689233, 5);
+    });
+});
+
+describe("Test performance calculation with Relax", () => {
+    const difficulty = calculateDifficulty(mainBeatmap, {
+        mods: [new ModRelax()],
+    });
+
+    test("SS", () => {
+        const performance = calculatePerformance(difficulty);
+
+        expect(performance.aim).toBeCloseTo(15.406637432317359, 5);
+        expect(performance.tap).toBeCloseTo(0.00000964814814814815, 5);
+        expect(performance.accuracy).toBe(0);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(27.149798544590933, 5);
+        expect(performance.total).toBeCloseTo(29.84284685256789, 5);
+    });
+
+    test("1 miss", () => {
+        const performance = calculatePerformance(difficulty, { miss: 1 });
+
+        expect(performance.aim).toBeCloseTo(14.813221125855863, 5);
+        expect(performance.tap).toBeCloseTo(0.000009068490964122712, 5);
+        expect(performance.accuracy).toBe(0);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(25.796968539816664, 5);
+        expect(performance.total).toBeCloseTo(28.473698467777695, 5);
+    });
+
+    test("99% approximated", () => {
+        const performance = calculatePerformance(difficulty, {
+            accPercent: new Accuracy({
+                percent: 99,
+                nobjects: mainBeatmap.hitObjects.objects.length,
+            }),
+        });
+
+        expect(performance.aim).toBeCloseTo(10.341743105805755, 5);
+        expect(performance.tap).toBeCloseTo(0.00000614955234821296, 5);
+        expect(performance.accuracy).toBe(0);
+        expect(performance.flashlight).toBe(0);
+        expect(performance.visual).toBeCloseTo(16.970078825420845, 5);
+        expect(performance.total).toBeCloseTo(19.135733656161882, 5);
+    });
+});
+
+describe("Test performance calculation with NoFail", () => {
+    const difficulty = calculateDifficulty(mainBeatmap, {
+        mods: [new ModNoFail()],
+    });
+
+    test("SS", () => {
+        const performance = calculatePerformance(difficulty);
+
+        expect(performance.total).toBeCloseTo(75.33580825676417, 5);
+    });
+
+    test("1 miss", () => {
+        const performance = calculatePerformance(difficulty, { miss: 1 });
+
+        expect(performance.total).toBeCloseTo(70.07401165457041, 5);
+    });
+
+    test("99% approximated", () => {
+        const performance = calculatePerformance(difficulty, {
+            accPercent: new Accuracy({
+                percent: 99,
+                nobjects: mainBeatmap.hitObjects.objects.length,
+            }),
+        });
+
+        expect(performance.total).toBeCloseTo(66.45809909307407, 5);
+    });
+
+    test("1.5 tap penalty", () => {
+        const performance = calculatePerformance(difficulty, {
+            tapPenalty: 1.5,
+        });
+
+        expect(performance.total).toBeCloseTo(71.11432510297956, 5);
+    });
+});
+
+describe("Test performance calculation with ScoreV2", () => {
+    const difficulty = calculateDifficulty(mainBeatmap, {
+        mods: [new ModScoreV2()],
+    });
+
+    test("SS", () => {
+        const performance = calculatePerformance(difficulty);
+
+        expect(performance.accuracy).toBeCloseTo(19.155029601507906, 5);
+        expect(performance.total).toBeCloseTo(81.14202142166661, 5);
+    });
+
+    test("1 miss", () => {
+        const performance = calculatePerformance(difficulty, { miss: 1 });
+
+        expect(performance.accuracy).toBeCloseTo(18.769694179956314, 5);
+        expect(performance.total).toBeCloseTo(77.7798844953465, 5);
+    });
+
+    test("99% approximated", () => {
+        const performance = calculatePerformance(difficulty, {
+            accPercent: new Accuracy({
+                percent: 99,
+                nobjects: mainBeatmap.hitObjects.objects.length,
+            }),
+        });
+
+        expect(performance.accuracy).toBeCloseTo(16.947414434062768, 5);
+        expect(performance.total).toBeCloseTo(74.39215280236935, 5);
+    });
+});
+
+describe("Test performance calculation with custom statistics", () => {
+    // These tests can be removed in 3.0.
+    test("Empty custom statistics", () => {
+        const performance = calculatePerformance(
+            calculateDifficulty(mainBeatmap),
+            {
+                stats: new MapStats({
+                    ar: 9,
+                    isForceAR: true,
+                    speedMultiplier: 1.25,
+                    oldStatistics: false,
+                }),
+            }
+        );
+
+        expect(performance.aim).not.toBeNaN();
+        expect(performance.tap).not.toBeNaN();
+        expect(performance.accuracy).not.toBeNaN();
+        expect(performance.flashlight).not.toBeNaN();
+        expect(performance.visual).not.toBeNaN();
+        expect(performance.total).not.toBeNaN();
+    });
+
+    test("Not empty custom statistics", () => {
+        const performance = calculatePerformance(
+            calculateDifficulty(mainBeatmap),
+            {
+                stats: new MapStats(),
+            }
+        );
+
+        expect(performance.aim).not.toBeNaN();
+        expect(performance.tap).not.toBeNaN();
+        expect(performance.accuracy).not.toBeNaN();
+        expect(performance.flashlight).not.toBeNaN();
+        expect(performance.visual).not.toBeNaN();
+        expect(performance.total).not.toBeNaN();
+    });
+});
+
+test("Test negative OD performance calculation", () => {
+    const difficulty = calculateDifficulty(mainBeatmap, {
+        mods: [new ModFlashlight()],
+    });
+
+    difficulty.beatmap.difficulty.od = 1;
+
+    let performance = calculatePerformance(difficulty);
+
+    const {
+        aim: firstAim,
+        tap: firstTap,
+        flashlight: firstFlashlight,
+    } = performance;
+
+    // Intentionally set OD to negative.
+    difficulty.beatmap.difficulty.od = -1;
+
+    performance = calculatePerformance(difficulty);
+
+    const {
+        aim: secondAim,
+        tap: secondTap,
+        flashlight: secondFlashlight,
+    } = performance;
+
+    expect(secondAim).toBeLessThan(firstAim);
+    expect(secondTap).toBeLessThan(firstTap);
+    expect(secondFlashlight).toBeLessThan(firstFlashlight);
+});
+
+test("Test spinner-only beatmap performance calculation", () => {
+    const performance = calculatePerformance(
+        calculateDifficulty(spinnerOnlyBeatmap, {
+            mods: [new ModFlashlight()],
+        })
+    );
+
+    expect(performance.accuracy).toBe(0);
+});
+
+test("Test effective miss count approximation", () => {
+    const performance = calculatePerformance(calculateDifficulty(mainBeatmap), {
+        combo: 50,
+    });
+
+    expect(performance.aim).not.toBeNaN();
+    expect(performance.tap).not.toBeNaN();
+    expect(performance.accuracy).not.toBeNaN();
+    expect(performance.flashlight).not.toBeNaN();
+    expect(performance.visual).not.toBeNaN();
+    expect(performance.total).not.toBeNaN();
+});
+
+test("Test string concatenation", () => {
+    const performance = calculatePerformance(calculateDifficulty(mainBeatmap));
+
+    expect(performance.toString()).toBe(
+        "75.34 pp (15.41 aim, 12.19 tap, 13.80 acc, 0.00 flashlight, 27.15 visual)"
+    );
+});
