673d627f516cf5415fb0703aaa6f1ba7e162f806
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-10 16:53:39 +0700

    feat(osu-rebalance-difficulty-calculator; DifficultyHitObjectCreator): add test

---
+++ packages/osu-rebalance-difficulty-calculator/tests/preprocessing/DifficultyHitObjectCreator.test.ts
@@ -0,0 +1,43 @@
+import {
+    Slider,
+    Vector2,
+    objectTypes,
+    SliderPath,
+    PathType,
+    modes,
+} from "@rian8337/osu-base";
+import { DifficultyHitObjectCreator } from "../../src";
+
+test("Test extremely fast sliders", () => {
+    const objects = new DifficultyHitObjectCreator().generateDifficultyObjects({
+        objects: [
+            new Slider({
+                startTime: 3000,
+                position: new Vector2(300, 100),
+                mapSliderVelocity: 1,
+                mapTickRate: 1,
+                msPerBeat: 60,
+                nodeSamples: [],
+                repetitions: 1,
+                speedMultiplier: 1,
+                tickDistanceMultiplier: 1,
+                type: objectTypes.slider,
+                path: new SliderPath({
+                    pathType: PathType.Linear,
+                    controlPoints: [new Vector2(300.0001, 100)],
+                    expectedDistance: 0.0001,
+                }),
+            }),
+        ],
+        circleSize: 4,
+        mode: modes.droid,
+        speedMultiplier: 1,
+    });
+
+    const object = objects[0];
+
+    expect(object.startTime).toBeCloseTo(object.endTime);
+    expect(object.object.stackedPosition).toEqual(
+        (<Slider>object.object).lazyEndPosition
+    );
+});
