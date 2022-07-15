1b9f6f13ff73560b3c02baea15572efa83c0a4d5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-07-15 22:56:54 +0700

    feat(osu-rebalance-difficulty-calculator; DroidVisualEvaluator): increase high AR bonus

--- packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidVisualEvaluator.ts
+++ packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidVisualEvaluator.ts
@@ -71,7 +71,7 @@ export abstract class DroidVisualEvaluator {
 
         if (current.timePreempt < 400) {
             // Give bonus for AR higher than 10.33.
-            strain += Math.pow(400 - current.timePreempt, 1.3) / 135;
+            strain += Math.pow(400 - current.timePreempt, 1.3) / 100;
         }
 
         if (current.object instanceof Slider) {
