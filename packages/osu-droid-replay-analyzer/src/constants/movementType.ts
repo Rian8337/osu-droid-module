b347cf1ec82861a7edcdd1259d05feed7ef0332d
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-09-14 18:07:43 +0700

    docs(osu-droid-replay-analyzer; movementType): JSDoc

--- packages/osu-droid-replay-analyzer/src/constants/movementType.ts
+++ packages/osu-droid-replay-analyzer/src/constants/movementType.ts
@@ -1,8 +1,19 @@
 /**
- * Movement type of a cursor in an osu!droid replay.
+ * Movement types of a cursor in an osu!droid replay.
+ *
+ * The cursor movement is represented as a player's action on the screen.
  */
 export enum movementType {
+    /**
+     * The player places their finger on the screen.
+     */
     DOWN,
+    /**
+     * The player drags their finger on the screen.
+     */
     MOVE,
+    /**
+     * The player releases their finger from the screen.
+     */
     UP,
 }
