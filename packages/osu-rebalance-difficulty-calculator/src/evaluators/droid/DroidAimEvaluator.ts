167c1b144fe5823750a87988ac97e40f7d99e41d
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-12-20 20:50:21 +0800

    feat(osu-rebalance-difficulty-calculator): global aim buff for droid

--- packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidAimEvaluator.ts
+++ packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidAimEvaluator.ts
@@ -6,6 +6,10 @@ import { AimEvaluator } from "../base/AimEvaluator";
  * An evaluator for calculating osu!droid Aim skill.
  */
 export abstract class DroidAimEvaluator extends AimEvaluator {
+    protected static override readonly wideAngleMultiplier: number = 1.65;
+    protected static override readonly sliderMultiplier: number = 1.5;
+    protected static override readonly velocityChangeMultiplier: number = 0.85;
+
     /**
      * Spacing threshold for a single hitobject spacing.
      */
