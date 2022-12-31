bd5b6a76e9d2ec77f0720dbbce2b3dd220c1483a
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-12-31 18:19:47 +0800

    docs(osu-base; Interpolation): document method

--- packages/osu-base/src/mathutil/Interpolation.ts
+++ packages/osu-base/src/mathutil/Interpolation.ts
@@ -1,4 +1,15 @@
+/**
+ * Holds interpolation methods for numbers.
+ */
 export abstract class Interpolation {
+    /**
+     * Performs a linear interpolation.
+     *
+     * @param start The starting point of the interpolation.
+     * @param final The final point of the interpolation.
+     * @param amount The interpolation multiplier.
+     * @returns The interpolated value.
+     */
     static lerp(start: number, final: number, amount: number): number {
         return start + (final - start) * amount;
     }
