5f3130892363355fa8b840057f9e17f85d590166
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-02-09 06:05:53 +0800

    feat(osu-rebalance-difficulty-calculator; DroidAimEvaluator): remove tick requirement

--- packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidAimEvaluator.ts
+++ packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidAimEvaluator.ts
@@ -69,11 +69,7 @@ export abstract class DroidAimEvaluator extends AimEvaluator {
             current.lazyJumpDistance / current.strainTime;
 
         // But if the last object is a slider, then we extend the travel velocity through the slider into the current object.
-        if (
-            last.object instanceof Slider &&
-            last.object.ticks > 0 &&
-            withSliders
-        ) {
+        if (last.object instanceof Slider && withSliders) {
             // Calculate the slider velocity from slider head to slider end.
             const travelVelocity: number =
                 last.travelDistance / last.travelTime;
@@ -94,11 +90,7 @@ export abstract class DroidAimEvaluator extends AimEvaluator {
         // As above, do the same for the previous hitobject.
         let prevVelocity: number = last.lazyJumpDistance / last.strainTime;
 
-        if (
-            lastLast.object instanceof Slider &&
-            lastLast.object.ticks > 0 &&
-            withSliders
-        ) {
+        if (lastLast.object instanceof Slider && withSliders) {
             const travelVelocity: number =
                 lastLast.travelDistance / lastLast.travelTime;
 
