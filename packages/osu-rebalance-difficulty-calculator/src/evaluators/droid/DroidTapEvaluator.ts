16f89f84886278f85aec06b8de73a2c0525bf25c
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-03-09 12:53:06 +0800

    feat(osu-rebalance-difficulty-calculator; DroidTapEvaluator): increase deltatime cap effect

--- packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidTapEvaluator.ts
+++ packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidTapEvaluator.ts
@@ -56,10 +56,10 @@ export abstract class DroidTapEvaluator extends SpeedEvaluator {
             }
 
             // Cap deltatime to the OD 300 hitwindow.
-            // 0.58 is derived from making sure 260 BPM 1/4 OD5 streams aren't nerfed harshly, whilst 0.91 limits the effect of the cap.
+            // 0.63 is derived from making sure 240 BPM 1/4 OD5 streams aren't nerfed harshly, whilst 0.9 limits the effect of the cap.
             strainTime /= MathUtils.clamp(
-                strainTime / greatWindowFull / 0.58,
-                0.91,
+                strainTime / greatWindowFull / 0.63,
+                0.9,
                 1
             );
         }
