4284abf6760503bf16cfec7024d4af350c47d8a2
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-07-01 15:36:05 +0800

    Change development installation flow

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,34 @@
 import typescript from "@rollup/plugin-typescript";
 import externals from "rollup-plugin-node-externals";
+import dts from "rollup-plugin-dts";
 
 const PACKAGE_ROOT = process.cwd();
 
-export default {
-    input: `${PACKAGE_ROOT}/src/index.ts`,
-    plugins: [
-        typescript({
-            tsconfig: `${PACKAGE_ROOT}/tsconfig.json`,
-        }),
-        externals({
-            deps: true,
-        }),
-    ],
-    output: {
-        file: `${PACKAGE_ROOT}/dist/index.js`,
-        format: "cjs",
-        sourcemap: true,
-    }
-};
+export default [
+    {
+        plugins: [
+            typescript({
+                removeComments: false,
+                sourceMap: true,
+                tsconfig: `${PACKAGE_ROOT}/tsconfig.json`,
+            }),
+            externals({
+                deps: true,
+            }),
+        ],
+        input: `${PACKAGE_ROOT}/src/index.ts`,
+        output: {
+            file: `${PACKAGE_ROOT}/dist/index.js`,
+            format: "cjs",
+            sourcemap: true,
+        },
+    },
+    {
+        plugins: [dts()],
+        input: `${PACKAGE_ROOT}/src/index.ts`,
+        output: {
+            file: `${PACKAGE_ROOT}/typings/index.d.ts`,
+            format: "cjs",
+        },
+    },
+];
