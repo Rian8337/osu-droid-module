5c3639fbb26ca7f33d8f2bb1fccdbaa2d4aaf8ae
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-02-25 18:14:17 +0700

    Add game mode to `IModApplicableToHitObject`

--- packages/osu-base/src/mods/IModApplicableToHitObject.ts
+++ packages/osu-base/src/mods/IModApplicableToHitObject.ts
@@ -1,4 +1,5 @@
 import { HitObject } from "../beatmap/hitobjects/HitObject";
+import { Modes } from "../constants/Modes";
 
 /**
  * An interface for mods that can be applied to hitobjects.
@@ -7,7 +8,8 @@ export interface IModApplicableToHitObject {
     /**
      * Applies this mod to a hitobject.
      *
+     * @param mode The mode to apply the mod for.
      * @param hitObject The hitobject to apply the mod to.
      */
-    applyToHitObject(hitObject: HitObject): void;
+    applyToHitObject(mode: Modes, hitObject: HitObject): void;
 }
