0125f0ec71d8e522079fd8b6e75ba2ed90f3f433
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-06-14 10:17:26 +0800

    Add vector linear interpolation method

--- packages/osu-base/src/math/Interpolation.ts
+++ packages/osu-base/src/math/Interpolation.ts
@@ -1,16 +1,44 @@
+import { Vector2 } from "./Vector2";
+
 /**
- * Holds interpolation methods for numbers.
+ * Holds interpolation methods for numbers and vectors.
  */
 export abstract class Interpolation {
     /**
-     * Performs a linear interpolation.
+     * Performs a linear interpolation of numbers.
      *
      * @param start The starting point of the interpolation.
      * @param final The final point of the interpolation.
      * @param amount The interpolation multiplier.
      * @returns The interpolated value.
      */
-    static lerp(start: number, final: number, amount: number): number {
-        return start + (final - start) * amount;
+    static lerp(start: number, final: number, amount: number): number;
+
+    /**
+     * Performs a linear interpolation of vectors.
+     *
+     * @param start The starting point of the interpolation.
+     * @param final The final point of the interpolation.
+     * @param amount The interpolation multiplier.
+     * @returns The interpolated vector.
+     */
+    static lerp(start: Vector2, final: Vector2, amount: number): Vector2;
+
+    static lerp(
+        start: number | Vector2,
+        final: number | Vector2,
+        amount: number,
+    ): number | Vector2 {
+        if (start instanceof Vector2 && final instanceof Vector2) {
+            return new Vector2(
+                this.lerp(start.x, final.x, amount),
+                this.lerp(start.y, final.y, amount),
+            );
+        } else {
+            return (
+                (start as number) +
+                ((final as number) - (start as number)) * amount
+            );
+        }
     }
 }
--- packages/osu-base/tests/math/Interpolation.test.ts
+++ packages/osu-base/tests/math/Interpolation.test.ts
@@ -1,7 +1,21 @@
-import { Interpolation } from "../../src";
+import { Interpolation, Vector2 } from "../../src";
 
-test("Test linear interpolation", () => {
+test("Test linear interpolation of numbers", () => {
     expect(Interpolation.lerp(4, 5, 2)).toBe(6);
     expect(Interpolation.lerp(2, 5, 2)).toBe(8);
     expect(Interpolation.lerp(-1, 7, 2)).toBe(15);
+});
+
+test("Test linear interpolation of vectors", () => {
+    expect(Interpolation.lerp(new Vector2(4), new Vector2(5), 2)).toEqual(
+        new Vector2(6),
+    );
+
+    expect(Interpolation.lerp(new Vector2(2, 3), new Vector2(5, 6), 2)).toEqual(
+        new Vector2(8, 9),
+    );
+
+    expect(
+        Interpolation.lerp(new Vector2(-1, 2), new Vector2(7, 8), 2),
+    ).toEqual(new Vector2(15, 14));
 });
