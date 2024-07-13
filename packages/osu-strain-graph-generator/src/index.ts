bf101ac4e32c167ee158256766843e677f093dec
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-07-14 03:04:37 +0800

    Overhaul strain generator
    - Update to canvas 3.0.0-rc2 for now to support worker threads
    - Change function parameters to not be fully dependent on calculator

--- packages/osu-strain-graph-generator/src/index.ts
+++ packages/osu-strain-graph-generator/src/index.ts
@@ -1,94 +1,74 @@
-import { ModFlashlight, Vector2 } from "@rian8337/osu-base";
-import {
-    DifficultyAttributes,
-    DifficultyCalculator,
-    DifficultyHitObject,
-} from "@rian8337/osu-difficulty-calculator";
-import {
-    DifficultyAttributes as RebalanceDifficultyAttributes,
-    DifficultyCalculator as RebalanceDifficultyCalculator,
-    DifficultyHitObject as RebalanceDifficultyHitObject,
-} from "@rian8337/osu-rebalance-difficulty-calculator";
+import { Beatmap, Vector2 } from "@rian8337/osu-base";
+import { StrainPeaks } from "@rian8337/osu-difficulty-calculator";
+import { StrainPeaks as RebalanceStrainPeaks } from "@rian8337/osu-rebalance-difficulty-calculator";
 import { loadImage } from "canvas";
 import { Chart } from "./Chart";
 
 /**
- * Generates the strain chart of a difficulty calculator and returns the chart as a buffer.
+ * Generates the strain chart of a beatmap and returns the chart as a buffer.
  *
- * @param calculator The difficulty calculator to generate the strain graph for.
+ * @param beatmap The beatmap to generate the strain graph for.
+ * @param strainPeaks The strain peaks of the beatmap.
+ * @param clockRate The clock rate of the beatmap.
  * @param beatmapsetID The beatmapset ID to get background image from. If omitted, the background will be plain white.
  * @param color The color of the graph.
  */
-export default async function getStrainChart(
-    calculator:
-        | DifficultyCalculator<DifficultyHitObject, DifficultyAttributes>
-        | RebalanceDifficultyCalculator<
-              RebalanceDifficultyHitObject,
-              RebalanceDifficultyAttributes
-          >,
+export default async function (
+    beatmap: Beatmap,
+    strainPeaks: StrainPeaks | RebalanceStrainPeaks,
+    clockRate: number,
     beatmapsetID?: number,
     color: string = "#000000",
-): Promise<Buffer | null> {
-    if (
-        [
-            calculator.strainPeaks.aimWithSliders.length,
-            calculator.strainPeaks.aimWithoutSliders.length,
-            calculator.strainPeaks.speed.length,
-            calculator.strainPeaks.flashlight.length,
-        ].some((v) => v === 0)
-    ) {
-        return null;
-    }
-
-    const sectionLength: number = 400;
-
-    const currentSectionEnd: number =
-        Math.ceil(
-            calculator.beatmap.hitObjects.objects[0].startTime / sectionLength,
-        ) * sectionLength;
+): Promise<Buffer> {
+    const sectionLength = 400;
+    const currentSectionEnd =
+        Math.ceil(beatmap.hitObjects.objects[0].startTime / sectionLength) *
+        sectionLength;
 
     const strainInformations: {
         readonly time: number;
         readonly strain: number;
     }[] = new Array(
         Math.max(
-            calculator.strainPeaks.aimWithSliders.length,
-            calculator.strainPeaks.speed.length,
-            calculator.strainPeaks.flashlight.length,
-        ),
+            strainPeaks.aimWithSliders.length,
+            strainPeaks.speed.length,
+            strainPeaks.flashlight.length,
+        ) + 1,
     );
 
-    for (let i = 0; i < strainInformations.length; ++i) {
-        const aimStrain: number = calculator.strainPeaks.aimWithSliders[i] ?? 0;
-        const speedStrain: number = calculator.strainPeaks.speed[i] ?? 0;
-        const flashlightStrain: number =
-            calculator.strainPeaks.flashlight[i] ?? 0;
+    // Intentionally insert a 0 strain at 400ms less than the beginning
+    // of the first object to smoothen the end curve.
+    strainInformations[0] = {
+        strain: 0,
+        time: (currentSectionEnd - sectionLength) / 1000,
+    };
+
+    for (let i = 1; i < strainInformations.length; ++i) {
+        const aimStrain = strainPeaks.aimWithSliders[i] ?? 0;
+        const speedStrain = strainPeaks.speed[i] ?? 0;
+        const flashlightStrain = strainPeaks.flashlight[i] ?? 0;
 
         strainInformations[i] = {
-            time: (currentSectionEnd + sectionLength * i) / 1000,
-            strain: calculator.mods.some((m) => m instanceof ModFlashlight)
-                ? (aimStrain + speedStrain + flashlightStrain) / 3
-                : (aimStrain + speedStrain) / 2,
+            time: (currentSectionEnd + sectionLength * (i - 1)) / 1000,
+            strain:
+                strainPeaks.flashlight.length > 0
+                    ? (aimStrain + speedStrain + flashlightStrain) / 3
+                    : (aimStrain + speedStrain) / 2,
         };
     }
 
-    const maxTime: number =
-        strainInformations.at(-1)!.time ??
-        calculator.objects.at(-1)!.object.endTime / 1000;
-    const maxStrain: number = Math.max(
-        ...strainInformations.map((v) => {
-            return v.strain;
-        }),
-        1,
-    );
+    const maxTime =
+        strainInformations.at(-1)?.time ??
+        beatmap.hitObjects.objects.at(-1)!.endTime / 1000 / clockRate;
+    const maxStrain = Math.max(...strainInformations.map((v) => v.strain), 1);
 
-    const maxXUnits: number = 10;
-    const maxYUnits: number = 10;
+    const maxXUnits = 10;
+    const maxYUnits = 10;
 
-    const unitsPerTickX: number = Math.ceil(maxTime / maxXUnits / 10) * 10;
-    const unitsPerTickY: number = Math.ceil(maxStrain / maxYUnits / 20) * 20;
+    const unitsPerTickX = Math.ceil(maxTime / maxXUnits / 10) * 10;
+    const unitsPerTickY = Math.ceil(maxStrain / maxYUnits / 20) * 20;
 
-    const chart: Chart = new Chart({
+    const chart = new Chart({
         graphWidth: 900,
         graphHeight: 250,
         minX: 0,
@@ -99,9 +79,7 @@ export default async function getStrainChart(
         unitsPerTickY,
         background: await loadImage(
             `https://assets.ppy.sh/beatmaps/${beatmapsetID}/covers/cover.jpg`,
-        ).catch(() => {
-            return undefined;
-        }),
+        ).catch(() => undefined),
         xLabel: "Time",
         yLabel: "Strain",
         pointRadius: 0,
