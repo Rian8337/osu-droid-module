a93f9a0691a5c230415643601ce8ab64f67d9b37
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-08-30 21:32:33 +0700

    Flip objects vertically when applying HR mod

--- packages/osu-base/src/mods/ModHardRock.ts
+++ packages/osu-base/src/mods/ModHardRock.ts
@@ -1,8 +1,14 @@
+import { HitObject } from "../beatmap/hitobjects/HitObject";
+import { Slider } from "../beatmap/hitobjects/Slider";
 import { BeatmapDifficulty } from "../beatmap/sections/BeatmapDifficulty";
 import { Modes } from "../constants/Modes";
+import { Vector2 } from "../math/Vector2";
 import { CircleSizeCalculator } from "../utils/CircleSizeCalculator";
+import { Playfield } from "../utils/Playfield";
+import { SliderPath } from "../utils/SliderPath";
 import { IModApplicableToDifficulty } from "./IModApplicableToDifficulty";
 import { IModApplicableToDroid } from "./IModApplicableToDroid";
+import { IModApplicableToHitObject } from "./IModApplicableToHitObject";
 import { IModApplicableToOsu } from "./IModApplicableToOsu";
 import { Mod } from "./Mod";
 
@@ -14,7 +20,8 @@ export class ModHardRock
     implements
         IModApplicableToDroid,
         IModApplicableToOsu,
-        IModApplicableToDifficulty
+        IModApplicableToDifficulty,
+        IModApplicableToHitObject
 {
     override readonly acronym = "HR";
     override readonly name = "HardRock";
@@ -50,6 +57,37 @@ export class ModHardRock
         difficulty.ar = this.applySetting(difficulty.ar);
         difficulty.od = this.applySetting(difficulty.od);
         difficulty.hp = this.applySetting(difficulty.hp);
+    }
+
+    applyToHitObject(_: Modes, hitObject: HitObject): void {
+        // Reflect the position of the hit object.
+        hitObject.position = this.reflectVector(hitObject.position);
+
+        if (!(hitObject instanceof Slider)) {
+            return;
+        }
+
+        // Reflect the control points of the slider. This will reflect the positions of head and tail circles.
+        hitObject.path = new SliderPath({
+            pathType: hitObject.path.pathType,
+            controlPoints: hitObject.path.controlPoints.map((v) =>
+                this.reflectControlPoint(v),
+            ),
+            expectedDistance: hitObject.path.expectedDistance,
+        });
+
+        // Reflect the position of slider ticks and repeats.
+        hitObject.nestedHitObjects.forEach((obj) => {
+            obj.position = this.reflectVector(obj.position);
+        });
+    }
+
+    private reflectVector(vector: Vector2): Vector2 {
+        return new Vector2(vector.x, Playfield.baseSize.y - vector.y);
+    }
+
+    private reflectControlPoint(vector: Vector2): Vector2 {
+        return new Vector2(vector.x, -vector.y);
     }
 
     private applySetting(value: number, ratio = 1.4): number {
