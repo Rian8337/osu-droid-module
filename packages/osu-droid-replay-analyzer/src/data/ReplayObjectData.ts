c8c1a3f10265f5ff9aa08e510a6b2665a1ad8166
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-11-06 20:10:30 +0800

    Improve `accuracy` comment

--- packages/osu-droid-replay-analyzer/src/data/ReplayObjectData.ts
+++ packages/osu-droid-replay-analyzer/src/data/ReplayObjectData.ts
@@ -9,10 +9,16 @@ import { HitResult } from "../constants/HitResult";
  */
 export class ReplayObjectData {
     /**
-     * For circles, this is the offset at which the circle was hit. If the hit accuracy is 10000, it means the circle was tapped too late ([game source code](https://github.com/osudroid/osu-droid/blob/6306c68e3ffaf671eac794bf45cc95c0f3313a82/src/ru/nsu/ccfit/zuev/osu/game/HitCircle.java#L298-L306)).
+     * For circles, this is the offset at which the circle was hit. If the hit accuracy is 10000,
+     * it means the circle was tapped too late and therefore the player missed ([game source code](https://github.com/osudroid/osu-droid/blob/ca0e4a2c06b9db18d094a15a4abf3f7ffcb05d7a/src/ru/nsu/ccfit/zuev/osu/game/GameplayHitCircle.java#L305)).
      *
-     * For sliders, this is the offset at which the slider head was hit. For
-     * sliderbreaks, the accuracy would be `Math.floor(<hit window 50>ms) + 13ms` ([game source code](https://github.com/osudroid/osu-droid/blob/6306c68e3ffaf671eac794bf45cc95c0f3313a82/src/ru/nsu/ccfit/zuev/osu/game/Slider.java#L821)).
+     * For sliders, this is the offset at which the slider head was hit. For sliderbreaks, there are two scenarios:
+     * - a value equal to `Math.floor(<hit window 50>ms) + 13ms` strictly for replays before version 1.7.2 ([game source code](https://github.com/osudroid/osu-droid/blob/6306c68e3ffaf671eac794bf45cc95c0f3313a82/src/ru/nsu/ccfit/zuev/osu/game/Slider.java#L821)), or
+     * - a value that is more than the slider's span duration ([game source code](https://github.com/osudroid/osu-droid/blob/ca0e4a2c06b9db18d094a15a4abf3f7ffcb05d7a/src/ru/nsu/ccfit/zuev/osu/game/GameplaySlider.java#L701)).
+     *
+     * `tickset` and `result` can be used in conjunction with this value to derive whether the player sliderbroke in the slider.
+     * If `result` is a 300, the player did not sliderbreak. Otherwise, if all slider ticks, repeats, and ends were hit, the
+     * player sliderbroke. In other cases, it is almost impossible to derive this result.
      *
      * For spinners, this is the total amount at which the spinner was spinned:
      * ```js
