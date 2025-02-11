ec9a674e945364f589ed7b6fbf2538444b3b8a92
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-02-11 18:07:55 +0800

    Add support for easing interpolation

--- packages/osu-base/src/math/Interpolation.ts
+++ packages/osu-base/src/math/Interpolation.ts
@@ -1,3 +1,4 @@
+import { Easing } from "../constants/Easing";
 import { Vector2 } from "./Vector2";
 
 /**
@@ -39,6 +40,217 @@ export abstract class Interpolation {
                 (start as number) +
                 ((final as number) - (start as number)) * amount
             );
+        }
+    }
+
+    /**
+     * Interpolates a value using an easing function.
+     *
+     * @param easing The easing function to use.
+     * @param t The progress of the interpolation, from 0 to 1.
+     * @returns The interpolated value.
+     */
+    static easing(easing: Easing, t: number): number {
+        switch (easing) {
+            case Easing.none:
+                return t;
+
+            case Easing.out:
+            case Easing.outQuad:
+                return t * (2 - t);
+
+            case Easing.in:
+            case Easing.inQuad:
+                return t * t;
+
+            case Easing.inOutQuad:
+                return t < 0.5 ? t * t * 2 : --t * t * -2 + 1;
+
+            case Easing.inCubic:
+                return t * t * t;
+
+            case Easing.outCubic:
+                return --t * t * t + 1;
+
+            case Easing.inOutCubic:
+                return t < 0.5 ? t * t * t * 4 : --t * t * t * 4 + 1;
+
+            case Easing.inQuart:
+                return t * t * t * t;
+
+            case Easing.outQuart:
+                return 1 - --t * t * t * t;
+
+            case Easing.inOutQuart:
+                return t < 0.5 ? t * t * t * t * 8 : --t * t * t * t * -8 + 1;
+
+            case Easing.inQuint:
+                return t * t * t * t * t;
+
+            case Easing.outQuint:
+                return --t * t * t * t * t + 1;
+
+            case Easing.inOutQuint:
+                return t < 0.5
+                    ? t * t * t * t * t * 16
+                    : --t * t * t * t * t * 16 + 1;
+
+            case Easing.inSine:
+                return 1 - Math.cos((t * Math.PI) / 2);
+
+            case Easing.outSine:
+                return Math.sin((t * Math.PI) / 2);
+
+            case Easing.inOutSine:
+                return 0.5 - 0.5 * Math.cos(Math.PI * t);
+
+            case Easing.inExpo:
+                return t === 0 ? 0 : Math.pow(2, 10 * t - 10);
+
+            case Easing.outExpo:
+                return t === 1 ? 1 : 1 - Math.pow(2, -10 * t);
+
+            case Easing.inOutExpo:
+                if (t === 0) {
+                    return 0;
+                }
+
+                if (t === 1) {
+                    return 1;
+                }
+
+                if ((t *= 2) < 1) {
+                    return 0.5 * Math.pow(2, 10 * t - 10);
+                }
+
+                return 0.5 * (2 - Math.pow(2, -10 * --t));
+
+            case Easing.inCirc:
+                return 1 - Math.sqrt(1 - t * t);
+
+            case Easing.outCirc:
+                return Math.sqrt(1 - --t * t);
+
+            case Easing.inOutCirc:
+                return (t *= 2) < 1
+                    ? 0.5 - 0.5 * Math.sqrt(1 - t * t)
+                    : 0.5 * Math.sqrt(1 - (t -= 2) * t) + 0.5;
+
+            case Easing.inElastic:
+                return (
+                    -Math.pow(2, -10 + 10 * t) *
+                    Math.sin((1 - 0.3 / 4 - t) * ((2 * Math.PI) / 0.3))
+                );
+
+            case Easing.outElastic:
+                return (
+                    Math.pow(2, -10 * t) *
+                        Math.sin((t - 0.3 / 4) * ((2 * Math.PI) / 0.3)) +
+                    1
+                );
+
+            case Easing.outElasticHalf:
+                return (
+                    Math.pow(2, -10 * t) *
+                        Math.sin((0.5 * t - 0.3 / 4) * ((2 * Math.PI) / 0.3)) +
+                    1
+                );
+
+            case Easing.outElasticQuarter:
+                return (
+                    Math.pow(2, -10 * t) *
+                        Math.sin((0.25 * t - 0.3 / 4) * ((2 * Math.PI) / 0.3)) +
+                    1
+                );
+
+            case Easing.inOutElastic:
+                if (t === 0) {
+                    return 0;
+                }
+
+                if (t === 1) {
+                    return 1;
+                }
+
+                if ((t *= 2) < 1) {
+                    return (
+                        -0.5 *
+                        Math.pow(2, -10 + 10 * t) *
+                        Math.sin(
+                            ((1 - (0.3 / 4) * 1.5 - t) *
+                                ((2 * Math.PI) / 0.3)) /
+                                1.5,
+                        )
+                    );
+                }
+
+                return (
+                    0.5 *
+                        Math.pow(2, -10 * --t) *
+                        Math.sin(
+                            ((t - (0.3 / 4) * 1.5) * ((2 * Math.PI) / 0.3)) /
+                                1.5,
+                        ) +
+                    1
+                );
+
+            case Easing.inBack:
+                return t * t * ((1.70158 + 1) * t - 1.70158);
+
+            case Easing.outBack:
+                return --t * t * ((1.70158 + 1) * t + 1.70158) + 1;
+
+            case Easing.inOutBack:
+                return (t *= 2) < 1
+                    ? 0.5 *
+                          t *
+                          t *
+                          ((1.70158 * 1.525 + 1) * t - 1.70158 * 1.525)
+                    : 0.5 *
+                          ((t -= 2) *
+                              t *
+                              ((1.70158 * 1.525 + 1) * t + 1.70158 * 1.525) +
+                              2);
+
+            case Easing.inBounce:
+                t = 1 - t;
+
+                if (t < 1 / 2.75) {
+                    return 1 - 7.5625 * t * t;
+                }
+
+                if (t < 2 / 2.75) {
+                    return 1 - (7.5625 * (t -= 1.5 / 2.75) * t + 0.75);
+                }
+
+                if (t < 2.5 / 2.75) {
+                    return 1 - (7.5625 * (t -= 2.25 / 2.75) * t + 0.9375);
+                }
+
+                return 1 - (7.5625 * (t -= 2.625 / 2.75) * t + 0.984375);
+
+            case Easing.outBounce:
+                if (t < 1 / 2.75) {
+                    return 7.5625 * t * t;
+                }
+
+                if (t < 2 / 2.75) {
+                    return 7.5625 * (t -= 1.5 / 2.75) * t + 0.75;
+                }
+
+                if (t < 2.5 / 2.75) {
+                    return 7.5625 * (t -= 2.25 / 2.75) * t + 0.9375;
+                }
+
+                return 7.5625 * (t -= 2.625 / 2.75) * t + 0.984375;
+
+            case Easing.inOutBounce:
+                return t < 0.5
+                    ? 0.5 - 0.5 * this.easing(Easing.outBounce, 1 - t * 2)
+                    : 0.5 * this.easing(Easing.outBounce, (t - 0.5) * 2) + 0.5;
+
+            case Easing.outPow10:
+                return --t * Math.pow(t, 10) + 1;
         }
     }
 }
--- packages/osu-base/tests/math/Interpolation.test.ts
+++ packages/osu-base/tests/math/Interpolation.test.ts
@@ -1,4 +1,4 @@
-import { Interpolation, Vector2 } from "../../src";
+import { Easing, Interpolation, Vector2 } from "../../src";
 
 test("Test linear interpolation of numbers", () => {
     expect(Interpolation.lerp(4, 5, 2)).toBe(6);
@@ -18,4 +18,190 @@ test("Test linear interpolation of vectors", () => {
     expect(
         Interpolation.lerp(new Vector2(-1, 2), new Vector2(7, 8), 2),
     ).toEqual(new Vector2(15, 14));
+});
+
+describe("Test interpolation using easing functions", () => {
+    const testEasing = (easing: Easing, t: number, expected: number) => {
+        expect(Interpolation.easing(easing, t)).toBeCloseTo(expected);
+    };
+
+    test("Easing.none", () => {
+        testEasing(Easing.none, 0.5, 0.5);
+        testEasing(Easing.none, 0.75, 0.75);
+    });
+
+    test("Easing.out", () => {
+        testEasing(Easing.out, 0.5, 0.75);
+        testEasing(Easing.out, 0.75, 0.9375);
+    });
+
+    test("Easing.outQuad", () => {
+        testEasing(Easing.outQuad, 0.5, 0.75);
+        testEasing(Easing.outQuad, 0.75, 0.9375);
+    });
+
+    test("Easing.in", () => {
+        testEasing(Easing.in, 0.5, 0.25);
+        testEasing(Easing.in, 0.75, 0.5625);
+    });
+
+    test("Easing.inQuad", () => {
+        testEasing(Easing.inQuad, 0.5, 0.25);
+        testEasing(Easing.inQuad, 0.75, 0.5625);
+    });
+
+    test("Easing.inOutQuad", () => {
+        testEasing(Easing.inOutQuad, 0.5, 0.5);
+        testEasing(Easing.inOutQuad, 0.75, 0.875);
+    });
+
+    test("Easing.inCubic", () => {
+        testEasing(Easing.inCubic, 0.5, 0.125);
+        testEasing(Easing.inCubic, 0.75, 0.421875);
+    });
+
+    test("Easing.outCubic", () => {
+        testEasing(Easing.outCubic, 0.5, 0.875);
+        testEasing(Easing.outCubic, 0.75, 0.984375);
+    });
+
+    test("Easing.inOutCubic", () => {
+        testEasing(Easing.inOutCubic, 0.5, 0.5);
+        testEasing(Easing.inOutCubic, 0.75, 0.9375);
+    });
+
+    test("Easing.inQuart", () => {
+        testEasing(Easing.inQuart, 0.5, 0.0625);
+        testEasing(Easing.inQuart, 0.75, 0.31640625);
+    });
+
+    test("Easing.outQuart", () => {
+        testEasing(Easing.outQuart, 0.5, 0.9375);
+        testEasing(Easing.outQuart, 0.75, 0.99609375);
+    });
+
+    test("Easing.inOutQuart", () => {
+        testEasing(Easing.inOutQuart, 0.5, 0.5);
+        testEasing(Easing.inOutQuart, 0.75, 0.96875);
+    });
+
+    test("Easing.inQuint", () => {
+        testEasing(Easing.inQuint, 0.5, 0.03125);
+        testEasing(Easing.inQuint, 0.75, 0.2373046875);
+    });
+
+    test("Easing.outQuint", () => {
+        testEasing(Easing.outQuint, 0.5, 0.96875);
+        testEasing(Easing.outQuint, 0.75, 0.9990234375);
+    });
+
+    test("Easing.inOutQuint", () => {
+        testEasing(Easing.inOutQuint, 0.5, 0.5);
+        testEasing(Easing.inOutQuint, 0.75, 0.984375);
+    });
+
+    test("Easing.inSine", () => {
+        testEasing(Easing.inSine, 0.5, 0.292893);
+        testEasing(Easing.inSine, 0.75, 0.6173165676349102);
+    });
+
+    test("Easing.outSine", () => {
+        testEasing(Easing.outSine, 0.5, 0.707107);
+        testEasing(Easing.outSine, 0.75, 0.9238795325112867);
+    });
+
+    test("Easing.inOutSine", () => {
+        testEasing(Easing.inOutSine, 0.5, 0.5);
+        testEasing(Easing.inOutSine, 0.75, 0.853553);
+    });
+
+    test("Easing.inExpo", () => {
+        testEasing(Easing.inExpo, 0.5, 0.03125);
+        testEasing(Easing.inExpo, 0.75, 0.17677669529663687);
+    });
+
+    test("Easing.outExpo", () => {
+        testEasing(Easing.outExpo, 0.5, 0.96875);
+        testEasing(Easing.outExpo, 0.75, 0.99447572827198);
+    });
+
+    test("Easing.inOutExpo", () => {
+        testEasing(Easing.inOutExpo, 0.5, 0.5);
+        testEasing(Easing.inOutExpo, 0.75, 0.984375);
+    });
+
+    test("Easing.inCirc", () => {
+        testEasing(Easing.inCirc, 0.5, 0.1339745962155614);
+        testEasing(Easing.inCirc, 0.75, 0.3385621722338523);
+    });
+
+    test("Easing.outCirc", () => {
+        testEasing(Easing.outCirc, 0.5, 0.8660254037844386);
+        testEasing(Easing.outCirc, 0.75, 0.9682458365518543);
+    });
+
+    test("Easing.inOutCirc", () => {
+        testEasing(Easing.inOutCirc, 0.5, 0.5);
+        testEasing(Easing.inOutCirc, 0.75, 0.9330127018922193);
+    });
+
+    test("Easing.inElastic", () => {
+        testEasing(Easing.inElastic, 0.5, -0.015624999999999993);
+        testEasing(Easing.inElastic, 0.75, 0.08838834764831859);
+    });
+
+    test("Easing.outElastic", () => {
+        testEasing(Easing.outElastic, 0.5, 1.015625);
+        testEasing(Easing.outElastic, 0.75, 1.00552427172802);
+    });
+
+    test("Easing.outElasticHalf", () => {
+        testEasing(Easing.outElasticHalf, 0.5, 0.984375);
+        testEasing(Easing.outElasticHalf, 0.75, 1);
+    });
+
+    test("Easing.outElasticQuarter", () => {
+        testEasing(Easing.outElasticQuarter, 0.5, 1.0270632938682638);
+        testEasing(Easing.outElasticQuarter, 0.75, 1.00390625);
+    });
+
+    test("Easing.inOutElastic", () => {
+        testEasing(Easing.inOutElastic, 0.5, 0.5);
+        testEasing(Easing.inOutElastic, 0.75, 0.988030555576266);
+    });
+
+    test("Easing.inBack", () => {
+        testEasing(Easing.inBack, 0.5, -0.08769750000000004);
+        testEasing(Easing.inBack, 0.75, 0.1825903124999999);
+    });
+
+    test("Easing.outBack", () => {
+        testEasing(Easing.outBack, 0.5, 1.0876975);
+        testEasing(Easing.outBack, 0.75, 1.0641365625);
+    });
+
+    test("Easing.inOutBack", () => {
+        testEasing(Easing.inOutBack, 0.5, 0.5);
+        testEasing(Easing.inOutBack, 0.75, 1.09968184375);
+    });
+
+    test("Easing.inBounce", () => {
+        testEasing(Easing.inBounce, 0.5, 0.234375);
+        testEasing(Easing.inBounce, 0.75, 0.52734375);
+    });
+
+    test("Easing.outBounce", () => {
+        testEasing(Easing.outBounce, 0.5, 0.765625);
+        testEasing(Easing.outBounce, 0.75, 0.97265625);
+    });
+
+    test("Easing.inOutBounce", () => {
+        testEasing(Easing.inOutBounce, 0.5, 0.5);
+        testEasing(Easing.inOutBounce, 0.75, 0.8828125);
+    });
+
+    test("Easing.outPow10", () => {
+        testEasing(Easing.outPow10, 0.5, 0.9999999999999999);
+        testEasing(Easing.outPow10, 0.75, 1);
+    });
 });
