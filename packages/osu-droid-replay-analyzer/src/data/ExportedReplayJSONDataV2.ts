a10323304200175a4b986c42e2a74d432ce73c64
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-02-26 17:55:09 +0700

    Remove `perfect` from replay JSON v2

--- packages/osu-droid-replay-analyzer/src/data/ExportedReplayJSONDataV2.ts
+++ packages/osu-droid-replay-analyzer/src/data/ExportedReplayJSONDataV2.ts
@@ -3,7 +3,8 @@ import { ExportedReplayJSONDataV1 } from "./ExportedReplayJSONDataV1";
 /**
  * The structure of the exported replay JSON data for version 2.
  */
-export interface ExportedReplayJSONDataV2 extends ExportedReplayJSONDataV1 {
+export interface ExportedReplayJSONDataV2
+    extends Omit<ExportedReplayJSONDataV1, "perfect"> {
     /**
      * The MD5 hash of the beatmap.
      */
