c352528de0aa898fc73c3826bbd91fe38a684e5b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-05 18:20:06 +0800

    Add `Playfield.center constant

--- packages/osu-base/src/utils/Playfield.ts
+++ packages/osu-base/src/utils/Playfield.ts
@@ -8,4 +8,9 @@ export abstract class Playfield {
      * The size of the playfield, which is 512x384.
      */
     static readonly baseSize = new Vector2(512, 384);
+
+    /**
+     * The center of the playfield, which is at (256, 192).
+     */
+    static readonly center = this.baseSize.scale(0.5);
 }
