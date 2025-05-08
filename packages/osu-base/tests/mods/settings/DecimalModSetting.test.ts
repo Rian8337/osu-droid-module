8bc497bdd1f58cdc399d221392bdb3eb04bf14b2
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-05-08 21:47:22 +0800

    Add tests for `DecimalModSetting` and `NumberModSetting`

---
+++ packages/osu-base/tests/mods/settings/DecimalModSetting.test.ts
@@ -0,0 +1,45 @@
+import { DecimalModSetting } from "../../../src";
+
+test("Test boundaries", () => {
+    // Test precision < 0
+    expect(() => new DecimalModSetting("Test", 0.12, 0, 1, 0.1, -1)).toThrow();
+});
+
+test("Test step with precision", () => {
+    const setting = new DecimalModSetting(
+        "Test",
+        0.12,
+        undefined,
+        undefined,
+        0.12,
+        2,
+    );
+
+    // These need to be exactly equal to prevent serialization differences.
+    expect(setting.value).toBe(0.12);
+
+    setting.value = 0.24;
+    expect(setting.value).toBe(0.24);
+
+    setting.value = 0.36;
+    expect(setting.value).toBe(0.36);
+
+    setting.value = 0.48;
+    expect(setting.value).toBe(0.48);
+});
+
+test("Test value cap", () => {
+    const setting = new DecimalModSetting("Test", 0.12, 0.12, 1.2, 0.12, 2);
+
+    // These need to be exactly equal to prevent serialization differences.
+    expect(setting.value).toBe(0.12);
+
+    setting.value = 0;
+    expect(setting.value).toBe(0.12);
+
+    setting.value = 1.2;
+    expect(setting.value).toBe(1.2);
+
+    setting.value = 1.5;
+    expect(setting.value).toBe(1.2);
+});
---
+++ packages/osu-base/tests/mods/settings/NumberModSetting.test.ts
@@ -0,0 +1,48 @@
+import { NumberModSetting } from "../../../src";
+
+test("Test boundaries", () => {
+    const expectThrows = (action: () => void) =>
+        expect(() => action()).toThrow();
+
+    // Test min > max
+    expectThrows(() => new NumberModSetting("Test", 0.12, 0.12, 0.1, 0));
+
+    // Test defaultValue > max
+    expectThrows(() => new NumberModSetting("Test", 0.24, 0.1, 0.12, 0));
+
+    // Test defaultValue < min
+    expectThrows(() => new NumberModSetting("Test", 0.1, 0.12, 0.24, 0));
+
+    // Test step < 0
+    expectThrows(() => new NumberModSetting("Test", 0.12, 0, 1, -0.1));
+});
+
+test("Test step", () => {
+    const setting = new NumberModSetting("Test", 0.12, 0.12, 1.2, 0.12);
+
+    expect(setting.value).toBeCloseTo(0.12, 5);
+
+    setting.value = 0.24;
+    expect(setting.value).toBeCloseTo(0.24, 5);
+
+    setting.value = 0.36;
+    expect(setting.value).toBeCloseTo(0.36, 5);
+
+    setting.value = 0.48;
+    expect(setting.value).toBeCloseTo(0.48, 5);
+});
+
+test("Test value cap", () => {
+    const setting = new NumberModSetting("Test", 0.12, 0.12, 1.2, 0.12);
+
+    expect(setting.value).toBeCloseTo(0.12, 5);
+
+    setting.value = 0;
+    expect(setting.value).toBeCloseTo(0.12, 5);
+
+    setting.value = 1.2;
+    expect(setting.value).toBeCloseTo(1.2, 5);
+
+    setting.value = 1.5;
+    expect(setting.value).toBeCloseTo(1.2, 5);
+});
