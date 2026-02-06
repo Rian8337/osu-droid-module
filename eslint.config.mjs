b4cc4621e8daf68b6f425b954ca18604c41faca1
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-02-06 20:06:38 +0800

    Migrate ESLint and Prettier configurations to their files

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+    {
+        ignores: [
+            "**/dist/",
+            "**/node_modules/",
+            "testing.js",
+            "*.md",
+            "*.osu",
+        ],
+    },
+    eslint.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+                ...globals.es2022,
+            },
+        },
+    },
+    {
+        files: ["**/*.ts"],
+        extends: [
+            ...tseslint.configs.strictTypeChecked,
+            ...tseslint.configs.stylisticTypeChecked,
+        ],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/no-extraneous-class": "off",
+        },
+    },
+    {
+        files: [
+            "eslint.config.mjs",
+            "prettier.config.mjs",
+            "rollup.config.mjs",
+        ],
+        ...tseslint.configs.disableTypeChecked,
+    },
+);
