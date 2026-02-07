c8e23f289f1c5be55a64f56a117de59d49612ade
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-02-07 22:11:02 +0800

    Add normal CDF and PDF for normal distribution

--- packages/osu-base/src/math/NormalDistribution.ts
+++ packages/osu-base/src/math/NormalDistribution.ts
@@ -9,6 +9,50 @@ import { ErrorFunction } from "./ErrorFunction";
  */
 export abstract class NormalDistribution {
     /**
+     * Computes the cumulative distribution function (CDF) of the distribution at x, i.e. P(X ≤ x).
+     *
+     * @param mean The mean (μ) of the normal distribution.
+     * @param stdDev The standard deviation (σ) of the normal distribution. Range: σ ≥ 0.
+     * @param x The location at which to compute the cumulative distribution function.
+     * @returns The cumulative distribution at {@link x}.
+     */
+    static cdf(mean: number, stdDev: number, x: number): number {
+        if (stdDev < 0) {
+            throw new RangeError(
+                "Invalid parametrization for the distribution.",
+            );
+        }
+
+        if (mean === x && stdDev === 0) {
+            return 0;
+        }
+
+        return 0.5 * ErrorFunction.erfc((mean - x) / (stdDev * Math.SQRT2));
+    }
+
+    /**
+     * Computes the probability density of the distribution (PDF) at x, i.e. ∂P(X ≤ x)/∂x.
+     *
+     * In MATLAB, this is known as `normpdf`.
+     *
+     * @param mean The mean (μ) of the normal distribution.
+     * @param stdDev The standard deviation (σ) of the normal distribution. Range: σ ≥ 0.
+     * @param x The location at which to compute the density.
+     * @returns The density at {@link x}.
+     */
+    static pdf(mean: number, stdDev: number, x: number): number {
+        if (stdDev < 0) {
+            throw new RangeError(
+                "Invalid parametrization for the distribution.",
+            );
+        }
+
+        const d = (x - mean) / stdDev;
+
+        return Math.exp(-0.5 * d * d) / (stdDev * Math.sqrt(2 * Math.PI));
+    }
+
+    /**
      * Computes the inverse of the cumulative distribution function (InvCDF) for the distribution
      * at the given probability. This is also known as the quantile or percent point function.
      *
@@ -20,7 +64,7 @@ export abstract class NormalDistribution {
     static invCDF(mean: number, stdDev: number, p: number): number {
         if (stdDev < 0) {
             throw new RangeError(
-                "Invalid parametrization for the distribution."
+                "Invalid parametrization for the distribution.",
             );
         }
 
