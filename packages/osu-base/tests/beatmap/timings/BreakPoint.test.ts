a5177b1afe017b9a826bc76dfb282c67b77f0022
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-02-07 16:40:51 +0800

    Update `BreakPoint` tests

--- packages/osu-base/tests/beatmap/timings/BreakPoint.test.ts
+++ packages/osu-base/tests/beatmap/timings/BreakPoint.test.ts
@@ -1,10 +1,7 @@
 import { BreakPoint } from "../../../src";
 
 test("Test time coverage", () => {
-    const breakPoint = new BreakPoint({
-        startTime: 1000,
-        endTime: 3000,
-    });
+    const breakPoint = new BreakPoint(1000, 3000);
 
     expect(breakPoint.contains(2000)).toBe(true);
     expect(breakPoint.contains(500)).toBe(false);
@@ -14,12 +11,9 @@ test("Test time coverage", () => {
 });
 
 test("Test string concatenation", () => {
-    const breakPoint = new BreakPoint({
-        startTime: 1000,
-        endTime: 3000,
-    });
+    const breakPoint = new BreakPoint(1000, 3000);
 
     expect(breakPoint.toString()).toBe(
-        "Start time: 1000, end time: 3000, duration: 2000"
+        "Start time: 1000, end time: 3000, duration: 2000",
     );
 });
