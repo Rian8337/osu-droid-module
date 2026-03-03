a6d49a5a2401ee9362921959dca21f097ac185a2
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-04 00:27:31 +0700

    Adjust osu!droid aim rating multiplier

--- packages/osu-rebalance-difficulty-calculator/src/DroidRatingCalculator.ts
+++ packages/osu-rebalance-difficulty-calculator/src/DroidRatingCalculator.ts
@@ -19,7 +19,7 @@ export class DroidRatingCalculator {
             return 0;
         }
 
-        let aimRating = Math.pow(aimDifficultyValue, 0.58) * 0.0346;
+        let aimRating = Math.pow(aimDifficultyValue, 0.58) * 0.12;
 
         if (this.mods.has(ModRelax)) {
             aimRating *= 0.9;
