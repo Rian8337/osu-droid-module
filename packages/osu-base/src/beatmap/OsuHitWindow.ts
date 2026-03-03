4399fd27cb61e22959b359b6add66b13f1efd8c5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-03 16:06:23 +0700

    Apply flooring and half-millisecond adjustments to osu!standard hit windows

--- packages/osu-base/src/beatmap/OsuHitWindow.ts
+++ packages/osu-base/src/beatmap/OsuHitWindow.ts
@@ -4,45 +4,15 @@ import { HitWindow } from "./HitWindow";
  * Represents the hit window of osu!standard.
  */
 export class OsuHitWindow extends HitWindow {
-    /**
-     * Calculates the overall difficulty value of a great (300) hit window.
-     *
-     * @param value The value of the hit window, in milliseconds.
-     * @returns The overall difficulty value.
-     */
-    static greatWindowToOD(value: number): number {
-        return (80 - value) / 6;
-    }
-
-    /**
-     * Calculates the overall difficulty value of a good (100) hit window.
-     *
-     * @param value The value of the hit window, in milliseconds.
-     * @returns The overall difficulty value.
-     */
-    static okWindowToOD(value: number): number {
-        return (140 - value) / 8;
-    }
-
-    /**
-     * Calculates the overall difficulty value of a meh hit window.
-     *
-     * @param value The value of the hit window, in milliseconds.
-     * @returns The overall difficulty value.
-     */
-    static mehWindowToOD(value: number): number {
-        return (200 - value) / 10;
-    }
-
     override get greatWindow(): number {
-        return 80 - 6 * this.overallDifficulty;
+        return Math.floor(80 - 6 * this.overallDifficulty) - 0.5;
     }
 
     override get okWindow(): number {
-        return 140 - 8 * this.overallDifficulty;
+        return Math.floor(140 - 8 * this.overallDifficulty) - 0.5;
     }
 
     override get mehWindow(): number {
-        return 200 - 10 * this.overallDifficulty;
+        return Math.floor(200 - 10 * this.overallDifficulty) - 0.5;
     }
 }
