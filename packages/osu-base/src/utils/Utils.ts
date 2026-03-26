901e0bb43fd58f0866f4be188c6384c8cb51b32f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-26 09:13:39 +0700

    Add utility function to do binary search

--- packages/osu-base/src/utils/Utils.ts
+++ packages/osu-base/src/utils/Utils.ts
@@ -37,6 +37,53 @@ export abstract class Utils {
     }
 
     /**
+     * Performs a binary search on an array with a custom predicate function.
+     *
+     * The predicate function should return:
+     * - a negative number if the element is less than the target,
+     * - a positive number if the element is greater than the target, and
+     * - 0 if the element is equal to the target.
+     *
+     * @param array The array to search through. Must be sorted according to the predicate function.
+     * @param predicate The predicate function to determine the order of elements.
+     * @returns The result of the binary search.
+     */
+    static binarySearch<T>(
+        array: readonly T[],
+        predicate: (element: T) => number,
+    ): {
+        /**
+         * If `found` is `true`, this is the index of an element in the array for which the predicate returns 0.
+         * If `found` is `false`, this is the index at which an element for which the predicate returns 0 could be
+         * inserted while maintaining the sorted order of the array.
+         */
+        readonly index: number;
+
+        /**
+         * Whether an element for which the predicate returns 0 was found in the array.
+         */
+        readonly found: boolean;
+    } {
+        let left = 0;
+        let right = array.length - 1;
+
+        while (left <= right) {
+            const mid = left + ((right - left) >> 1);
+            const result = predicate(array[mid]);
+
+            if (result === 0) {
+                return { index: mid, found: true };
+            } else if (result < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return { index: left, found: false };
+    }
+
+    /**
      * Pauses the execution of a function for
      * the specified duration.
      *
