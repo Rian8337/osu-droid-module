72bb55787e5e1b4a0fadab956872aecc910ace86
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-04-15 19:22:38 +0800

    Reduce d/t^2 influence further

--- packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidSnapAimEvaluator.ts
+++ packages/osu-rebalance-difficulty-calculator/src/evaluators/droid/DroidSnapAimEvaluator.ts
@@ -265,7 +265,7 @@ export abstract class DroidSnapAimEvaluator {
 
     private static highBpmBonus(ms: number, distance: number): number {
         return (
-            (1 / (1 - Math.pow(0.03, Math.pow(ms / 1000, 0.75)))) *
+            (1 / (1 - Math.pow(0.03, Math.pow(ms / 1000, 0.65)))) *
             // Decrease bonus for distances less than radius. These patterns have little to no aim difficulty,
             // and some of them may have inflated bonus due to incredibly short delta times (e.g., doubles).
             MathUtils.smootherstep(
