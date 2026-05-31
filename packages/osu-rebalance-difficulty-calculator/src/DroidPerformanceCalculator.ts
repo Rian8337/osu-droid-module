6cf257fe344c4a89314dc6eb2c1026888b914d39
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-06-01 00:12:20 +0800

    Fix constant mismatch in `calculateEstimatedSliderBreaks`

--- packages/osu-rebalance-difficulty-calculator/src/DroidPerformanceCalculator.ts
+++ packages/osu-rebalance-difficulty-calculator/src/DroidPerformanceCalculator.ts
@@ -788,8 +788,10 @@ export class DroidPerformanceCalculator extends PerformanceCalculator<IDroidDiff
         );
 
         // Scores with more Oks and Mehs are more likely to have slider breaks.
+        // We add an arbitrary value to both sides of the division to make it more stable on extreme ends.
         const nonMissMistakeAdjustment =
-            (nonMissMistakes - estimatedSliderBreaks + 0.5) / nonMissMistakes;
+            (nonMissMistakes - estimatedSliderBreaks + 4.5) /
+            (nonMissMistakes + 4);
 
         // There is a low probability of extra slider breaks on effective miss counts close to 1, as
         // score based calculations are good at indicating if only a single break occurred.
