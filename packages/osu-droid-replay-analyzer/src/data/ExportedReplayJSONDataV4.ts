4d3e0636f65cca2729d08a8dad019ffc23ccbd74
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-07-30 08:54:28 +0800

    Add `sliderHeadHits` and `sliderRepeatHits` to exported replay JSON version 4

--- packages/osu-droid-replay-analyzer/src/data/ExportedReplayJSONDataV4.ts
+++ packages/osu-droid-replay-analyzer/src/data/ExportedReplayJSONDataV4.ts
@@ -5,6 +5,16 @@ import { ExportedReplayJSONDataV3 } from "./ExportedReplayJSONDataV3";
  */
 export interface ExportedReplayJSONDataV4 extends ExportedReplayJSONDataV3 {
     /**
+     * The amount of slider head hits in the replay.
+     */
+    sliderHeadHits: number | null;
+
+    /**
+     * The amount of slider repeat hits in the replay.
+     */
+    sliderRepeatHits: number | null;
+
+    /**
      * Whether {@link score} includes mod multipliers.
      */
     isScoreWithMultiplier?: boolean;
