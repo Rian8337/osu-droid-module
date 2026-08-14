a974a75457f904d51a166690993d23badb32ede5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-08-14 16:53:16 +0800

    Add `ModSuddenDeath.failOnSliderTail`

--- packages/osu-base/src/mods/ModSuddenDeath.ts
+++ packages/osu-base/src/mods/ModSuddenDeath.ts
@@ -3,14 +3,14 @@ import { IModApplicableToOsuStable } from "./IModApplicableToOsuStable";
 import { Mod } from "./Mod";
 import { ModNoFail } from "./ModNoFail";
 import { ModPerfect } from "./ModPerfect";
+import { BooleanModSetting } from "./settings/BooleanModSetting";
 
 /**
  * Represents the SuddenDeath mod.
  */
 export class ModSuddenDeath
     extends Mod
-    implements IModApplicableToDroid, IModApplicableToOsuStable
-{
+    implements IModApplicableToDroid, IModApplicableToOsuStable {
     override readonly acronym = "SD";
     override readonly name = "Sudden Death";
 
@@ -23,6 +23,16 @@ export class ModSuddenDeath
     readonly isOsuRelevant = true;
     readonly osuScoreMultiplier = 1;
     readonly bitwise = 1 << 5;
+
+    /**
+     * Whether a miss on a slider's tail should also trigger a fail.
+     */
+    readonly failOnSliderTail = new BooleanModSetting(
+        "Also fail when missing a slider tail",
+        "failOnSliderTail",
+        "Whether a miss on a slider's tail should also trigger a fail.",
+        false,
+    );
 
     constructor() {
         super();
